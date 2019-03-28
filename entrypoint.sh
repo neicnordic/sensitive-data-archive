d8ce07325e8982aebbf8335285654468b09181fe
Dmytro Titov <dmytro.titov@gmail.com>
2019-03-28 13:26:08 +0100

    Fix env-vars naming

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sed -i 's%MQ_USER%'${USER_NAME}'%g' /etc/rabbitmq/definitions.json
-sed -i 's%MQ_PASSWORD_HASH%'${PASSWORD_HASH}'%g' /etc/rabbitmq/definitions.json
+sed -i 's%MQ_USER%'${MQ_USER}'%g' /etc/rabbitmq/definitions.json
+sed -i 's%MQ_PASSWORD_HASH%'${MQ_PASSWORD_HASH}'%g' /etc/rabbitmq/definitions.json
 sed -i 's%CEGA_CONNECTION%'${CEGA_CONNECTION}'%g' /etc/rabbitmq/definitions.json
 sed -i 's%CEGA_CONNECTION%'${CEGA_CONNECTION}'%g' /etc/rabbitmq/advanced.config
 
