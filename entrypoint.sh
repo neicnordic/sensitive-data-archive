58a001edfb828d17248c3ab6a73753d93e88726a
Joakim Bygdell <Joakim.Bygdell@nbis.se>
2020-05-04 15:24:35 +0200

    Update entrypoint script
    Sets up the MQ for standalone configuration when the ENV `CEGA_CONNECTION`
    is absent

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,6 @@
 
 [ -z "${MQ_USER}" ] && echo 'Environment variable MQ_USER is empty' 1>&2 && exit 1
 [ -z "${MQ_PASSWORD_HASH}" ] && echo 'Environment variable MQ_PASSWORD_HASH is empty' 1>&2 && exit 1
-[ -z "${CEGA_CONNECTION}" ] && echo 'Environment variable CEGA_CONNECTION is empty' 1>&2 && exit 1
 
 if [ -z "${MQ_SERVER_CERT}" ] || [ -z "${MQ_SERVER_KEY}" ]; then
 SSL_SUBJ="/C=SE/ST=Sweden/L=Uppsala/O=NBIS/OU=SysDevs/CN=LocalEGA"
@@ -31,55 +30,132 @@ EOF
 
 chmod 600 "/var/lib/rabbitmq/rabbitmq.conf"
 
+if [ -n "${CEGA_CONNECTION}" ]; then
 cat > "/var/lib/rabbitmq/definitions.json" <<EOF
 {
   "users": [
     {
-      "name": "${MQ_USER}", "password_hash": "${MQ_PASSWORD_HASH}",
-      "hashing_algorithm": "rabbit_password_hashing_sha256", "tags": "administrator"
+      "name": "${MQ_USER}",
+      "password_hash": "${MQ_PASSWORD_HASH}",
+      "hashing_algorithm": "rabbit_password_hashing_sha256",
+      "tags": "administrator"
     }
   ],
   "vhosts": [
-    { "name": "${MQ_VHOST:-/}" }
+    {
+      "name": "${MQ_VHOST:-/}"
+    }
   ],
   "permissions": [
-    { "user": "${MQ_USER}", "vhost": "${MQ_VHOST:-/}", "configure": ".*", "write": ".*", "read": ".*" }
+    {
+      "user": "${MQ_USER}",
+      "vhost": "${MQ_VHOST:-/}",
+      "configure": ".*",
+      "write": ".*",
+      "read": ".*"
+    }
   ],
   "parameters": [
     {
-      "name": "CEGA-ids", "vhost": "${MQ_VHOST:-/}", "component": "federation-upstream",
-      "value": { "ack-mode": "on-confirm", "queue": "v1.stableIDs", "trust-user-id": false, "uri": "${CEGA_CONNECTION}" }
+      "name": "CEGA-ids",
+      "vhost": "${MQ_VHOST:-/}",
+      "component": "federation-upstream",
+      "value": {
+        "ack-mode": "on-confirm",
+        "queue": "v1.stableIDs",
+        "trust-user-id": false,
+        "uri": "${CEGA_CONNECTION}"
+      }
     },
     {
-      "name": "CEGA-files", "vhost": "${MQ_VHOST:-/}", "component": "federation-upstream",
-      "value": { "ack-mode": "on-confirm", "queue": "v1.files", "trust-user-id": false, "uri": "${CEGA_CONNECTION}" }
+      "name": "CEGA-files",
+      "vhost": "${MQ_VHOST:-/}",
+      "component": "federation-upstream",
+      "value": {
+        "ack-mode": "on-confirm",
+        "queue": "v1.files",
+        "trust-user-id": false,
+        "uri": "${CEGA_CONNECTION}"
+      }
     }
   ],
   "policies": [
     {
-      "vhost": "${MQ_VHOST:-/}", "name": "CEGA-files", "pattern": "files", "apply-to": "queues", "priority": 0,
-      "definition": { "federation-upstream": "CEGA-files" }
+      "vhost": "${MQ_VHOST:-/}",
+      "name": "CEGA-files",
+      "pattern": "files",
+      "apply-to": "queues",
+      "priority": 0,
+      "definition": {
+        "federation-upstream": "CEGA-files"
+      }
     },
     {
-      "vhost": "${MQ_VHOST:-/}", "name": "CEGA-ids", "pattern": "stableIDs", "apply-to": "queues", "priority": 0,
-      "definition": { "federation-upstream": "CEGA-ids" }
+      "vhost": "${MQ_VHOST:-/}",
+      "name": "CEGA-ids",
+      "pattern": "stableIDs",
+      "apply-to": "queues",
+      "priority": 0,
+      "definition": {
+        "federation-upstream": "CEGA-ids"
+      }
     }
   ],
   "queues": [
-    {"name": "stableIDs", "vhost": "${MQ_VHOST:-/}", "durable": true, "auto_delete": false, "arguments":{}},
-    {"name": "files",     "vhost": "${MQ_VHOST:-/}", "durable": true, "auto_delete": false, "arguments":{}},
-    {"name": "archived",  "vhost": "${MQ_VHOST:-/}", "durable": true, "auto_delete": false, "arguments":{}}
+    {
+      "name": "stableIDs",
+      "vhost": "${MQ_VHOST:-/}",
+      "durable": true,
+      "auto_delete": false,
+      "arguments": {}
+    },
+    {
+      "name": "files",
+      "vhost": "${MQ_VHOST:-/}",
+      "durable": true,
+      "auto_delete": false,
+      "arguments": {}
+    },
+    {
+      "name": "archived",
+      "vhost": "${MQ_VHOST:-/}",
+      "durable": true,
+      "auto_delete": false,
+      "arguments": {}
+    }
   ],
   "exchanges": [
-    {"name":"cega", "vhost":"${MQ_VHOST:-/}", "type":"topic", "durable":true, "auto_delete":false, "internal":false, "arguments":{}}, 
-    {"name":"lega", "vhost":"${MQ_VHOST:-/}", "type":"topic", "durable":true, "auto_delete":false, "internal":false, "arguments":{}}
-  ], 
+    {
+      "name": "cega",
+      "vhost": "${MQ_VHOST:-/}",
+      "type": "topic",
+      "durable": true,
+      "auto_delete": false,
+      "internal": false,
+      "arguments": {}
+    },
+    {
+      "name": "lega",
+      "vhost": "${MQ_VHOST:-/}",
+      "type": "topic",
+      "durable": true,
+      "auto_delete": false,
+      "internal": false,
+      "arguments": {}
+    }
+  ],
   "bindings": [
-    { "source":"lega", "vhost": "${MQ_VHOST:-/}", "destination":"archived", "destination_type":"queue", "routing_key":"archived", "arguments":{}}
+    {
+      "source": "lega",
+      "vhost": "${MQ_VHOST:-/}",
+      "destination": "archived",
+      "destination_type": "queue",
+      "routing_key": "archived",
+      "arguments": {}
+    }
   ]
 }
 EOF
-chmod 600 "/var/lib/rabbitmq/definitions.json"
 
 if [ -n "${MQ_VHOST}" ];then
 MQ_VHOST="/${MQ_VHOST}"
@@ -145,5 +221,164 @@ cat > "/var/lib/rabbitmq/advanced.config" <<EOF
 ].
 EOF
 chmod 600 "/var/lib/rabbitmq/advanced.config"
+else
+cat > "/var/lib/rabbitmq/definitions.json" <<EOF
+{
+  "rabbit_version": "3.7",
+  "users": [
+    {
+      "name": "${MQ_USER}",
+      "password_hash": "${MQ_PASSWORD_HASH}",
+      "hashing_algorithm": "rabbit_password_hashing_sha256",
+      "tags": "administrator"
+    }
+  ],
+  "vhosts": [
+    {
+      "name": "${MQ_VHOST:-/}"
+    }
+  ],
+  "permissions": [
+    {
+      "user": "${MQ_USER}",
+      "vhost": "${MQ_VHOST:-/}",
+      "configure": ".*",
+      "write": ".*",
+      "read": ".*"
+    }
+  ],
+  "parameters": [],
+  "global_parameters": [
+    {
+      "name": "cluster_name",
+      "value": "rabbit@localhost"
+    }
+  ],
+  "policies": [],
+  "queues": [
+    {
+      "name": "archived",
+      "vhost": "${MQ_VHOST:-/}",
+      "durable": true,
+      "auto_delete": false,
+      "arguments": {}
+    },
+    {
+      "name": "files",
+      "vhost": "${MQ_VHOST:-/}",
+      "durable": true,
+      "auto_delete": false,
+      "arguments": {}
+    },
+    {
+      "name": "files.completed",
+      "vhost": "${MQ_VHOST:-/}",
+      "durable": true,
+      "auto_delete": false,
+      "arguments": {}
+    },
+    {
+      "name": "files.error",
+      "vhost": "${MQ_VHOST:-/}",
+      "durable": true,
+      "auto_delete": false,
+      "arguments": {}
+    },
+    {
+      "name": "files.inbox",
+      "vhost": "${MQ_VHOST:-/}",
+      "durable": true,
+      "auto_delete": false,
+      "arguments": {}
+    },
+    {
+      "name": "files.processing",
+      "vhost": "${MQ_VHOST:-/}",
+      "durable": true,
+      "auto_delete": false,
+      "arguments": {}
+    },
+    {
+      "name": "stableIDs",
+      "vhost": "${MQ_VHOST:-/}",
+      "durable": true,
+      "auto_delete": false,
+      "arguments": {}
+    },
+  ],
+  "exchanges": [
+    {
+      "name": "lega",
+      "vhost": "${MQ_VHOST:-/}",
+      "type": "topic",
+      "durable": true,
+      "auto_delete": false,
+      "internal": false,
+      "arguments": {}
+    }
+  ],
+  "bindings": [
+    {
+      "source": "lega",
+      "vhost": "${MQ_VHOST:-/}",
+      "destination_type": "queue",
+      "arguments": {},
+      "destination": "archived",
+      "routing_key": "archived"
+    },
+    {
+      "source": "lega",
+      "vhost": "${MQ_VHOST:-/}",
+      "destination_type": "queue",
+      "arguments": {},
+      "destination": "files",
+      "routing_key": "files"
+    },
+    {
+      "source": "lega",
+      "vhost": "${MQ_VHOST:-/}",
+      "destination_type": "queue",
+      "arguments": {},
+      "destination": "files.completed",
+      "routing_key": "files.completed"
+    },
+    {
+      "source": "lega",
+      "vhost": "${MQ_VHOST:-/}",
+      "destination_type": "queue",
+      "arguments": {},
+      "destination": "files.error",
+      "routing_key": "files.error"
+    },
+    {
+      "source": "lega",
+      "vhost": "${MQ_VHOST:-/}",
+      "destination_type": "queue",
+      "arguments": {},
+      "destination": "files.inbox",
+      "routing_key": "files.inbox"
+    },
+    {
+      "source": "lega",
+      "vhost": "${MQ_VHOST:-/}",
+      "destination_type": "queue",
+      "arguments": {},
+      "destination": "files.processing",
+      "routing_key": "files.processing"
+    },
+    {
+      "source": "lega",
+      "vhost": "${MQ_VHOST:-/}",
+      "destination_type": "queue",
+      "arguments": {},
+      "destination": "stableIDs",
+      "routing_key": "stableIDs"
+    }
+  ]
+}
+EOF
+fi
+
+chmod 600 "/var/lib/rabbitmq/definitions.json"
 
 exec "$@"
