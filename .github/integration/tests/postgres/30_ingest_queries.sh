3f5ca2401535bc015566b02f6d12694d47c3c8e4
Joakim Bygdell <joakim.bygdell@nbis.se>
2023-06-12 11:52:01 +0200

    [integration tests] Update DB tests to match new functions

--- .github/integration/tests/postgres/30_ingest_queries.sh
+++ .github/integration/tests/postgres/30_ingest_queries.sh
@@ -1,31 +1,36 @@
 #!/bin/sh
 set -eou pipefail
 
-result=$(psql -U postgres -h postgres -d sda -At -c "SELECT submission_file_path,submission_user from sda.files;")
-path=$(echo "$result" | cut -d '|' -f1)
-user=$(echo "$result" | cut -d '|' -f2)
-
 export PGPASSWORD=ingest
+user="test-user"
+corrID="33d29907-c565-4a90-98b4-e31b992ab376"
 
 ## insert file
-resp=$(psql -U ingest -h postgres -d sda -At -c  "UPDATE local_ega.main SET status = 'IN_INGESTION' WHERE local_ega.main.submission_file_path = '$path' AND local_ega.main.submission_user = '$user' AND local_ega.main.status = 'INIT' RETURNING id;")
-if [ "$(echo "$resp" | tr -d '\n')" != "1UPDATE 1" ]; then
+fileID=$(psql -U ingest -h postgres -d sda -At -c "SELECT sda.register_file('inbox/test-file.c4gh', '$user');")
+if [ -z "$fileID" ]; then
+    echo "register_file failed"
+    exit 1
+fi
+
+resp=$(psql -U ingest -h postgres -d sda -At -c "INSERT INTO sda.file_event_log(file_id, event, correlation_id, user_id, message) VALUES('$fileID', 'submitted', '$corrID', '$user', '{}');")
+if [ "$(echo "$resp" | tr -d '\n')" != "INSERT 0 1" ]; then
     echo "insert file failed"
     exit 1
 fi
 
 ## store header
-resp=$(psql -U ingest -h postgres -d sda -At -c "UPDATE local_ega.files SET header = '637279707434676801000000010000006c00000000000000' WHERE id = 1;")
+resp=$(psql -U ingest -h postgres -d sda -At -c "UPDATE sda.files SET header = '637279707434676801000000010000006c00000000000000' WHERE id = '$fileID';")
 if [ "$resp" != "UPDATE 1" ]; then
     echo "store header failed"
     exit 1
 fi
+
 ## set archived
 archive_path=d853c51b-6aed-4243-b427-177f5e588857
 size="2035150"
 checksum="f03775a50feea74c579d459fdbeb27adafd543b87f6692703543a6ebe7daa1ff"
-resp=$(psql -U ingest -h postgres -d sda -At -c "UPDATE local_ega.files SET status = 'ARCHIVED', archive_path = '$archive_path', archive_filesize = '$size', inbox_file_checksum = '$checksum', inbox_file_checksum_type = 'SHA256' WHERE id = 1;")
-if [ "$resp" != "UPDATE 1" ]; then
+resp=$(psql -U ingest -h postgres -d sda -At -c "SELECT sda.set_archived('$fileID', '$corrID', '$archive_path', '$size', '$checksum', 'SHA256');")
+if [ "$resp" != "" ]; then
     echo "mark file archived failed"
     exit 1
 fi
--- .github/integration/tests/postgres/40_verify_queries.sh
+++ .github/integration/tests/postgres/40_verify_queries.sh
@@ -2,10 +2,19 @@
 set -eou pipefail
 
 export PGPASSWORD=verify
+corrID="33d29907-c565-4a90-98b4-e31b992ab376"
+fileID=$(psql -U verify -h postgres -d sda -At -c "SELECT DISTINCT file_id from sda.file_event_log WHERE correlation_id = '$corrID';")
+
+## get file status
+status=$(psql -U verify -h postgres -d sda -At -c "SELECT event from sda.file_event_log WHERE correlation_id = '$corrID' ORDER BY id DESC LIMIT 1;")
+if [ "$status" = "" ]; then
+    echo "get file status failed"
+    exit 1
+fi
 
 ## get file header
 header="637279707434676801000000010000006c00000000000000"
-dbheader=$(psql -U verify -h postgres -d sda -At -c "SELECT header from local_ega.files WHERE id = 1;")
+dbheader=$(psql -U verify -h postgres -d sda -At -c "SELECT header from sda.files WHERE id = '$fileID';")
 if [ "$dbheader" != "$header" ]; then
     echo "wrong header recieved"
     exit 1
@@ -13,11 +22,10 @@ fi
 
 ## mark file as 'COMPLETED'
 archive_checksum="64e56b0d245b819c116b5f1ad296632019490b57eeaebb419a5317e24a153852"
-archive_size="2035150"
 decrypted_size="2034254"
 decrypted_checksum="febee6829a05772eea93c647e38bf5cc5bf33d1bcd0ea7d7bdd03225d84d2553"
-resp=$(psql -U verify -h postgres -d sda -At -c "UPDATE local_ega.files SET status = 'COMPLETED', archive_filesize = '$archive_size', archive_file_checksum = '$archive_checksum', archive_file_checksum_type = 'SHA256', decrypted_file_size = '$decrypted_size', decrypted_file_checksum = '$decrypted_checksum', decrypted_file_checksum_type = 'SHA256' WHERE id = 1;")
-if [ "$resp" != "UPDATE 1" ]; then
-    echo "mark file ready failed"
+resp=$(psql -U verify -h postgres -d sda -At -c "SELECT sda.set_verified('$fileID', '$corrID', '$archive_checksum', 'SHA256', '$decrypted_size', '$decrypted_checksum', 'SHA256')")
+if [ "$resp" != "" ]; then
+    echo "set_verified failed"
     exit 1
 fi
