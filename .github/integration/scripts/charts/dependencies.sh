8286d6d70d6bda45dc60e90780810dd88e2d6482
Joakim Bygdell <joakim.bygdell@nbis.se>
2023-08-08 11:22:34 +0200

    Programatically get latest github release for crypt4gh

--- .github/integration/scripts/charts/dependencies.sh
+++ .github/integration/scripts/charts/dependencies.sh
@@ -2,7 +2,7 @@
 set -ex
 
 YQ_VERSION="v4.20.1"
-C4GH_VERSION="1.7.5"
+C4GH_VERSION="$(curl -sL https://api.github.com/repos/neicnordic/crypt4gh/releases/latest | jq -r '.name')"
 
 random-string() {
         head -c 32 /dev/urandom | base64 -w0 | tr -d '/+' | fold -w 32 | head -n 1
@@ -11,7 +11,7 @@ random-string() {
 sudo curl -sLO "https://github.com/mikefarah/yq/releases/download/${YQ_VERSION}/yq_linux_amd64" -O /usr/bin/yq &&
         sudo chmod +x /usr/bin/yq
 
-curl -sL https://github.com/neicnordic/crypt4gh/releases/download/v"${C4GH_VERSION}"/crypt4gh_linux_x86_64.tar.gz | sudo tar -xz -C /usr/bin/ &&
+curl -sL https://github.com/neicnordic/crypt4gh/releases/download/"${C4GH_VERSION}"/crypt4gh_linux_x86_64.tar.gz | sudo tar -xz -C /usr/bin/ &&
         sudo chmod +x /usr/bin/crypt4gh
 
 # secret for the crypt4gh keypair
