e549c0d8257fee88f790958452aabd09d9c0c00d
Joakim Bygdell <joakim.bygdell@nbis.se>
2023-08-22 10:52:25 +0200

    Use matrix to test multiple k8s versions

--- .github/integration/scripts/charts/k3d.sh
+++ .github/integration/scripts/charts/k3d.sh
@@ -4,11 +4,15 @@ set -ex
 k8s="$(curl -L -s https://dl.k8s.io/release/stable.txt)"
 
 curl -s -L https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | sudo bash
-curl -sLO https://storage.googleapis.com/kubernetes-release/release/"$k8s"/bin/linux/amd64/kubectl
+
+if [ -n "$1" ]; then
+    k8s=$(k3d version list k3s | grep "$1" | head -n 1 | cut -d '-' -f 1)
+fi
+
+curl -sLO https://dl.k8s.io/release/"$k8s"/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
 
-sudo k3d cluster create sda --image=rancher/k3s:"$k8s"-k3s1 --wait --timeout 10m
-sudo k3d kubeconfig merge sda --kubeconfig-switch-context
-mkdir -p ~/.kube/ && sudo cp /root/.k3d/kubeconfig-sda.yaml ~/.kube/config
-sudo chown $UID:$UID ~/.kube/config && chmod 600 ~/.kube/config
+k3d cluster create sda --image=rancher/k3s:"$k8s"-k3s1 --wait --timeout 10m
+k3d kubeconfig merge sda --kubeconfig-switch-context
+mkdir -p ~/.kube/ && cp ~/.config/kubeconfig-sda.yaml ~/.kube/config
