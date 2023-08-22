83bcb9a0ca929c6576a7aea4af2066b6a51a674a
Joakim Bygdell <joakim.bygdell@nbis.se>
2023-08-22 12:20:48 +0200

    Move chart tests into `PR container build` actiion

--- .github/integration/scripts/charts/deploy_charts.sh
+++ .github/integration/scripts/charts/deploy_charts.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 set -ex
 
+if [ -z "$2" ];then
+    echo "PR number missing"
+    exit 1
+fi
+
 if [ "$1" == "sda-db" ]; then
     ROOTPASS=$(yq e '.global.db.password' .github/integration/scripts/charts/values.yaml)
     helm install postgres charts/sda-db \
-        --set image.tag=test-postgres \
-        --set image.pullPolicy=Never \
+        --set image.tag="PR$2-postgres" \
+        --set image.pullPolicy=IfNotPresent \
         --set global.postgresAdminPassword="$ROOTPASS" \
         --set global.tls.enabled=false \
         --set persistence.enabled=false \
@@ -16,8 +21,8 @@ fi
 if [ "$1" == "sda-mq" ]; then
     ADMINPASS=$(yq e '.global.broker.password' .github/integration/scripts/charts/values.yaml)
     helm install broker charts/sda-mq \
-        --set image.tag=test-rabbitmq \
-        --set image.pullPolicy=Never \
+        --set image.tag="PR$2-rabbitmq" \
+        --set image.pullPolicy=IfNotPresent \
         --set global.adminPassword="$ADMINPASS" \
         --set global.adminUser=admin \
         --set global.tls.enabled=false \
@@ -28,8 +33,8 @@ fi
 
 if [ "$1" == "sda-svc" ]; then
     helm install pipeline charts/sda-svc \
-        --set image.tag=test \
-        --set image.pullPolicy=Never \
+        --set image.tag="PR$2" \
+        --set image.pullPolicy=IfNotPresent \
         -f .github/integration/scripts/charts/values.yaml \
         --wait
 fi
