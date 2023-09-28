c5efadc9f18194dafd7bd97eb3d77340aa2d905e
Joakim Bygdell <joakim.bygdell@nbis.se>
2023-09-28 10:24:24 +0200

    [internal] convert `helper_test` to run as a suite

--- sda/internal/helper/helper_test.go
+++ sda/internal/helper/helper_test.go
@@ -3,112 +3,125 @@ package helper
 import (
 	"fmt"
 	"os"
-	"testing"
 	"path/filepath"
+	"testing"
 
-	"github.com/stretchr/testify/assert"
 	"github.com/lestrrat-go/jwx/v2/jwk"
 	"github.com/lestrrat-go/jwx/v2/jws"
 	"github.com/lestrrat-go/jwx/v2/jwt"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 )
 
-func TestMakeFolder(t *testing.T) {
+type HelperTest struct {
+	suite.Suite
+}
+
+func TestUserAuthTestSuite(t *testing.T) {
+	suite.Run(t, new(HelperTest))
+}
+
+func (suite *HelperTest) SetupTest() {
+
+}
+
+func (suite *HelperTest) TestMakeFolder() {
 	privateK, publicK, _ := MakeFolder("dummy-folder")
-	assert.Equal(t, "dummy-folder/private-key", privateK)
-	assert.Equal(t, "dummy-folder/public-key", publicK)
+	assert.Equal(suite.T(), "dummy-folder/private-key", privateK)
+	assert.Equal(suite.T(), "dummy-folder/public-key", publicK)
 
 	defer os.RemoveAll("dummy-folder")
 }
 
-func TestCreateRSAkeys(t *testing.T) {
+func (suite *HelperTest) TestCreateRSAkeys() {
 	privateK, publicK, _ := MakeFolder("dummy-folder")
-	assert.Nil(t, CreateRSAkeys(privateK, publicK))
+	assert.Nil(suite.T(), CreateRSAkeys(privateK, publicK))
 
 	defer os.RemoveAll("dummy-folder")
 }
 
-func TestParsePrivateRSAKey(t *testing.T) {
+func (suite *HelperTest) TestParsePrivateRSAKey() {
 	privateK, publicK, _ := MakeFolder("dummy-folder")
 	e := CreateRSAkeys(privateK, publicK)
-	assert.Nil(t, e)
+	assert.Nil(suite.T(), e)
 	_, err := ParsePrivateRSAKey(privateK, "/dummy.ega.nbis.se")
-	assert.Nil(t, err)
+	assert.Nil(suite.T(), err)
 
 	defer os.RemoveAll("dummy-folder")
 }
 
-func TestCreateRSAToken(t *testing.T) {
+func (suite *HelperTest) TestCreateRSAToken() {
 	privateK, publicK, _ := MakeFolder("dummy-folder")
 	e := CreateRSAkeys(privateK, publicK)
-	assert.Nil(t, e)
+	assert.Nil(suite.T(), e)
 	ParsedPrKey, _ := ParsePrivateRSAKey(privateK, "/dummy.ega.nbis.se")
 	tok, err := CreateRSAToken(ParsedPrKey, "RS256", DefaultTokenClaims)
-	assert.Nil(t, err)
+	assert.Nil(suite.T(), err)
 
 	set := jwk.NewSet()
 	keyData, err := os.ReadFile(filepath.Join(filepath.Clean(publicK), "/dummy.ega.nbis.se.pub"))
-	assert.NoError(t, err)
+	assert.NoError(suite.T(), err)
 	key, err := jwk.ParseKey(keyData, jwk.WithPEM(true))
-	assert.NoError(t, err)
+	assert.NoError(suite.T(), err)
 	err = jwk.AssignKeyID(key)
-	assert.NoError(t, err)
+	assert.NoError(suite.T(), err)
 	set.AddKey(key)
 
 	fmt.Println(tok)
 	_, err = jwt.Parse([]byte(tok), jwt.WithKeySet(set, jws.WithInferAlgorithmFromKey(true)), jwt.WithValidate(true))
-	assert.NoError(t, err)
+	assert.NoError(suite.T(), err)
 
 	defer os.RemoveAll("dummy-folder")
 }
 
-func TestCreateECkeys(t *testing.T) {
+func (suite *HelperTest) TestCreateECkeys() {
 	privateK, publicK, _ := MakeFolder("dummy-folder")
-	assert.Nil(t, CreateECkeys(privateK, publicK))
+	assert.Nil(suite.T(), CreateECkeys(privateK, publicK))
 
 	defer os.RemoveAll("dummy-folder")
 }
 
-func TestParsePrivateECKey(t *testing.T) {
+func (suite *HelperTest) TestParsePrivateECKey() {
 	privateK, publicK, _ := MakeFolder("dummy-folder")
 	e := CreateECkeys(privateK, publicK)
-	assert.Nil(t, e)
+	assert.Nil(suite.T(), e)
 
 	k, err := ParsePrivateECKey(privateK, "/dummy.ega.nbis.se")
-	assert.Nil(t, err)
-	assert.Equal(t, "EC", fmt.Sprintf("%v", k.KeyType()))
+	assert.Nil(suite.T(), err)
+	assert.Equal(suite.T(), "EC", fmt.Sprintf("%v", k.KeyType()))
 
 	defer os.RemoveAll("dummy-folder")
 }
 
-func TestCreateECToken(t *testing.T) {
+func (suite *HelperTest) TestCreateECToken() {
 	privateK, publicK, _ := MakeFolder("dummy-folder")
 	e := CreateECkeys(privateK, publicK)
-	assert.Nil(t, e)
+	assert.Nil(suite.T(), e)
 	ParsedPrKey, err := ParsePrivateECKey(privateK, "/dummy.ega.nbis.se")
-	assert.NoError(t, err)
+	assert.NoError(suite.T(), err)
 	_, err = CreateECToken(ParsedPrKey, "ES256", DefaultTokenClaims)
-	assert.Nil(t, err)
+	assert.Nil(suite.T(), err)
 
 	defer os.RemoveAll("dummy-folder")
 }
 
-func TestCreateHSToken(t *testing.T) {
+func (suite *HelperTest) TestCreateHSToken() {
 	key := make([]byte, 256)
 	tok, err := CreateHSToken(key, "HS256", DefaultTokenClaims)
-	assert.Nil(t, err)
+	assert.Nil(suite.T(), err)
 
 	set := jwk.NewSet()
 	jwtKey, err := jwk.FromRaw(key)
-	assert.NoError(t, err)
+	assert.NoError(suite.T(), err)
 
 	err = jwk.AssignKeyID(jwtKey)
-	assert.NoError(t, err)
-	
+	assert.NoError(suite.T(), err)
+
 	set.AddKey(jwtKey)
 
 	fmt.Println(tok)
 	_, err = jwt.Parse([]byte(tok), jwt.WithKeySet(set, jws.WithInferAlgorithmFromKey(true)), jwt.WithValidate(true))
-	assert.NoError(t, err)
+	assert.NoError(suite.T(), err)
 
 	defer os.RemoveAll("dummy-folder")
 }
