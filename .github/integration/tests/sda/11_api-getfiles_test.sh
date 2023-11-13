08f2846aa7c28b641cd43507aec48d07ba520872
MalinAhlberg <malin.ahlberg@nbis.se>
2023-11-13 11:01:18 +0100

    break out api test

---
+++ .github/integration/tests/sda/11_api-getfiles_test.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -e
+
+# Test the API files endpoint
+token="$(curl http://oidc:8080/tokens | jq -r '.[0]')"
+curl -k -L "http://api:8080/files" -H "Authorization: Bearer $token"
+response="$(curl -k -L "http://api:8080/files" -H "Authorization: Bearer $token" | jq -r 'sort_by(.inboxPath)|.[-1].fileStatus')"
+if [ "$response" != "uploaded" ]; then
+	echo "API returned incorrect value, expected ready got: $response"
+	exit 1
+fi
--- .github/integration/tests/sda/20_ingest-verify_test.sh
+++ .github/integration/tests/sda/20_ingest-verify_test.sh
@@ -68,12 +68,3 @@ until [ "$(curl -su guest:guest http://rabbitmq:15672/api/queues/sda/verified/ |
     fi
     sleep 2
 done
-
-# Test the API files endpoint
-token="$(curl http://oidc:8080/tokens | jq -r '.[0]')"
-curl -k -L "http://api:8080/files" -H "Authorization: Bearer $token"
-response="$(curl -k -L "http://api:8080/files" -H "Authorization: Bearer $token" | jq -r 'sort_by(.inboxPath)|.[-1].fileStatus')"
-if [ "$response" != "uploaded" ]; then
-	echo "API returned incorrect value, expected ready got: $response"
-	exit 1
-fi
