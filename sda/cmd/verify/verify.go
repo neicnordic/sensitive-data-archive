0651144fbf9a8167e5f419c2ad9dd27460166daa
Joakim Bygdell <joakim.bygdell@nbis.se>
2023-12-12 10:59:21 +0100

    [verify] Store file errors in the DB

--- sda/cmd/verify/verify.go
+++ sda/cmd/verify/verify.go
@@ -9,6 +9,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"strings"
 
 	"github.com/neicnordic/crypt4gh/streaming"
 	"github.com/neicnordic/sensitive-data-archive/internal/broker"
@@ -159,8 +160,29 @@ func main() {
 
 			var file database.FileInfo
 			file.Size, err = archive.GetFileSize(message.ArchivePath)
-			if err != nil {
+			if err != nil { //nolint:nestif
 				log.Errorf("Failed to get archived file size, reson: (%s)", err.Error())
+				if strings.Contains(err.Error(), "no such file or directory") || strings.Contains(err.Error(), "NoSuchKey:") || strings.Contains(err.Error(), "NotFound:") {
+					jsonMsg, _ := json.Marshal(map[string]string{"error": err.Error()})
+					if err := db.UpdateFileEventLog(message.FileID, "error", delivered.CorrelationId, "verify", string(jsonMsg), string(delivered.Body)); err != nil {
+						log.Errorf("failed to set ingestion status for file from message: %v", delivered.CorrelationId)
+					}
+				}
+
+				if err := delivered.Ack(false); err != nil {
+					log.Errorf("Failed to Ack message, reason: (%s)", err.Error())
+				}
+
+				// Send the message to an error queue so it can be analyzed.
+				fileError := broker.InfoError{
+					Error:           "Failed to get archived file size",
+					Reason:          err.Error(),
+					OriginalMessage: message,
+				}
+				body, _ := json.Marshal(fileError)
+				if err := mq.SendMessage(delivered.CorrelationId, conf.Broker.Exchange, "error", body); err != nil {
+					log.Errorf("failed to publish message, reason: (%s)", err.Error())
+				}
 
 				continue
 			}
@@ -169,7 +191,6 @@ func main() {
 			f, err := archive.NewFileReader(message.ArchivePath)
 			if err != nil {
 				log.Errorf("Failed to open archived file, reson: %v ", err.Error())
-
 				// Send the message to an error queue so it can be analyzed.
 				infoErrorMessage := broker.InfoError{
 					Error:           "Failed to open archived file",
