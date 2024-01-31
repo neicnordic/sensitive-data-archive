c20e9ee969a63e3396232222063abbb888b5f6b1
Joakim Bygdell <joakim.bygdell@nbis.se>
2024-01-31 10:58:28 +0100

    Delete `pipeline` folder

--- sda-pipeline/.github/integration/setup/posix/1_posix_env_setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sed -i 's/=s3/=posix/g' dev_utils/env.ingest
-sed -i 's/=s3/=posix/g' dev_utils/env.verify
-sed -i 's/=s3/=posix/g' dev_utils/env.backup
--- sda-pipeline/.github/integration/setup/sftpheader/1_sftp_env_setup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sed -i 's/BACKUP_TYPE=s3/BACKUP_TYPE=sftp/g' dev_utils/env.backup
