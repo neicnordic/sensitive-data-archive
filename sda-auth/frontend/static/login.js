bc6f5a98016a3bf7558e88859029eb089df0f608
Joakim Bygdell <joakim.bygdell@nbis.se>
2024-02-05 13:29:43 +0100

    Use images instead of buttons for the login options

--- sda-auth/frontend/static/login.js
+++ sda-auth/frontend/static/login.js
@@ -2,11 +2,8 @@ $.get('/login-options', (options) => {
     options.forEach(option => {
         $("#login-options").append(
             `<div class="col-sm py-3">
-      <p class="lead">
-        For logging in with ${option.Name} please click here:
-      </p>
-      <a class="btn btn-primary btn-lg btn-block" href="${option.URL}">
-        Log in with ${option.Name}
+      <a href="${option.URL}">
+        <img src="public/${option.Name}.png" class="img-fluid">
       </a>
     </div>
     `);
