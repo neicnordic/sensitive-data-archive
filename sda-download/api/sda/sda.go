ca576b9eac7f600466599916d69b394346e8ac5f
MalinAhlberg <malin.ahlberg@nbis.se>
2024-02-26 16:14:48 +0100

    pass on key http headers
    for interoperability with htsget

--- sda-download/api/sda/sda.go
+++ sda-download/api/sda/sda.go
@@ -241,6 +241,11 @@ func Download(c *gin.Context) {
 		c.Header("Content-Disposition", fmt.Sprintf("filename: %v", fileID))
 		c.Header("ETag", fileDetails.DecryptedChecksum)
 		c.Header("Last-Modified", lastModified.Format(http.TimeFormat))
+
+		// set the user and server public keys that is send from htsget
+		log.Debugf("Got to setting the headers: %s", c.GetHeader("client-public-key"))
+		c.Header("client-public-key", c.GetHeader("client-public-key"))
+		c.Header("server-public-key", c.GetHeader("server-public-key"))
 	}
 
 	if c.Request.Method == http.MethodHead {
