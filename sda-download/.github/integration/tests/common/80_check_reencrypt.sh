6f0db0efff34d1ace4a4ecc80abebaab3e6c4f59
nanjiangshu <nanjiang.shu@gmail.com>
2024-03-18 15:55:40 +0100

    add integration test for reencrypt

---
+++ sda-download/.github/integration/tests/common/80_check_reencrypt.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+if [ "$STORAGETYPE" = s3notls ]; then
+    exit 0
+fi
+
+cd dev_utils || exit 1
+
+# get a token, set up variables
+token=$(curl --cacert certs/ca.pem "https://localhost:8000/tokens" | jq -r  '.[0]')
+
+if [ -z "$token" ]; then
+    echo "Failed to obtain token"
+    exit 1
+fi
+
+dataset="https://doi.example/ty009.sfrrss/600.45asasga"
+file="dummy_data"
+expected_size=1048605
+
+# download decrypted full file,  check file size
+curl --cacert certs/ca.pem -H "Authorization: Bearer $token" "https://localhost:8443/s3/$dataset/$file" --output full1.bam
+
+if [ ! -f "full1.bam" ]; then
+    echo "Failed to download full1.bam"
+    exit 1
+fi
+
+file_size=$(stat -c %s full1.bam)  # Get the size of the file
+
+if [ "$file_size" -ne "$expected_size" ]; then
+    echo "Incorrect file size for full decrypted file"
+    exit 1
+fi
+
+C4GH_PASSPHRASE=strongpass # passphrase for the client crypt4gh key
+export C4GH_PASSPHRASE
+
+# test reencrypt the file header with the client public key 
+clientkey=$(base64 -w0 client.pub.pem)
+curl --cacert certs/ca.pem -H "Authorization: Bearer $token" -H "Client-Public-Key: $clientkey" "https://localhost:8443/s3-encrypted/$dataset/$file" --output reencrypted.bam
+if [ ! -f "reencrypted.bam" ]; then
+    echo "Failed to reencrypt the header of the file from sda-download"
+    exit 1
+fi
+
+# descrypt the reencrypted file
+if ! crypt4gh decrypt --sk client.sec.pem < reencrypted.bam > full2.bam; then
+    echo "Failed to descrypt the reencrypted BAM file with the client public key"
+    exit 1
+fi
+
+if ! cmp --silent full1.bam full2.bam; then
+    echo "Decrypted version of the reencrypted file and the original unencrypted file, are different"
+    exit 1
+fi
+
+# Clean up
+rm full1.bam reencrypted.bam full2.bam
