87f8a7fb66073716cf61a8be90031252db00005d
Joakim Bygdell <joakim.bygdell@nbis.se>
2024-08-14 07:26:58 +0200

    Update .github/integration/tests/sda/60_api_admin_test.sh
    Co-authored-by: Alex Aperis <76202622+aaperis@users.noreply.github.com>

--- .github/integration/tests/sda/60_api_admin_test.sh
+++ .github/integration/tests/sda/60_api_admin_test.sh
@@ -4,7 +4,7 @@ set -e
 token="$(curl http://oidc:8080/tokens | jq -r '.[0]')"
 result="$(curl -sk -L "http://api:8080/users/test@dummy.org/files" -H "Authorization: Bearer $token" | jq '. | length')"
 if [ "$result" -ne 2 ]; then
-    echo "wong number of files returned for user test@dummy.org"
+    echo "wrong number of files returned for user test@dummy.org"
     echo "expected 4 got $result"
     exit 1
 fi
