f3f24cfb56b2ce3d3cf1d3ba4ffd63fc31da4a50
nanjiangshu <nanjiang.shu@gmail.com>
2024-09-03 16:03:30 +0200

    add helping functions

---
+++ sda-admin/helpers/helpers.go
@@ -0,0 +1,148 @@
+package helpers
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/golang-jwt/jwt"
+)
+
+// Helper functions used by more than one module
+
+// CheckTokenExpiration is used to determine whether the token is expiring in less than a day
+func CheckTokenExpiration(accessToken string) error {
+	// Parse jwt token with unverifies, since we don't need to check the signatures here
+	token, _, err := new(jwt.Parser).ParseUnverified(accessToken, jwt.MapClaims{})
+	if err != nil {
+		return fmt.Errorf("could not parse token, reason: %s", err)
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok {
+		return fmt.Errorf("broken token (claims are empty): %v\nerror: %s", claims, err)
+	}
+
+	// Check if the token has exp claim
+	if claims["exp"] == nil {
+		return fmt.Errorf("could not parse token, reason: no expiration date")
+	}
+
+	// Parse the expiration date from token, handle cases where
+	//  the date format is nonstandard, e.g. test tokens are used
+	var expiration time.Time
+	switch iat := claims["exp"].(type) {
+	case float64:
+		expiration = time.Unix(int64(iat), 0)
+	case json.Number:
+		tmp, _ := iat.Int64()
+		expiration = time.Unix(tmp, 0)
+	case string:
+		i, err := strconv.ParseInt(iat, 10, 64)
+		if err != nil {
+			return fmt.Errorf("could not parse token, reason: %s", err)
+		}
+		expiration = time.Unix(int64(i), 0)
+	default:
+		return fmt.Errorf("could not parse token, reason: unknown expiration date format")
+	}
+
+	switch untilExp := time.Until(expiration); {
+	case untilExp < 0:
+		return fmt.Errorf("the provided access token has expired, please renew it")
+	case untilExp > 0 && untilExp < 24*time.Hour:
+		fmt.Fprintln(
+			os.Stderr,
+			"The provided access token expires in",
+			time.Until(expiration).Truncate(time.Second),
+		)
+		fmt.Fprintln(os.Stderr, "Consider renewing the token.")
+	default:
+		fmt.Fprintln(
+			os.Stderr,
+			"The provided access token expires in",
+			time.Until(expiration).Truncate(time.Second),
+		)
+	}
+
+	return nil
+}
+
+// necessary for mocking in unit tests
+var GetResponseBody = GetBody
+
+// GetBody gets the body of the response from the URL
+func GetBody(url, token string) ([]byte, error) {
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create the request, reason: %v", err)
+	}
+
+	// Add headers
+	req.Header.Add("Authorization", "Bearer "+token)
+	req.Header.Add("Content-Type", "application/json")
+
+	// Send the request
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get response, reason: %v", err)
+	}
+
+	// Check the status code
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("server returned status %d", res.StatusCode)
+	}
+
+	// Read the response body
+	resBody, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read response body, reason: %v", err)
+	}
+
+	defer res.Body.Close()
+
+	return resBody, nil
+}
+
+// necessary for mocking in unit tests
+var PostRequest = PostReq
+
+// PostReq sends a POST request to the server with a JSON body and returns the response body or an error.
+func PostReq(url, token string, jsonBody []byte) ([]byte, error) {
+	// Create a new POST request with the provided JSON body
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonBody))
+	if err != nil {
+		return nil, fmt.Errorf("failed to create the request, reason: %v", err)
+	}
+
+	// Add headers
+	req.Header.Add("Authorization", "Bearer "+token)
+	req.Header.Add("Content-Type", "application/json")
+
+	// Send the request
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("failed to send request, reason: %v", err)
+	}
+	defer res.Body.Close()
+
+	// Check the status code
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("server returned status %d", res.StatusCode)
+	}
+
+	// Read the response body
+	resBody, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read response body, reason: %v", err)
+	}
+
+	return resBody, nil
+}
---
+++ sda-admin/helpers/helpers_test.go
@@ -0,0 +1,109 @@
+package helpers
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"github.com/golang-jwt/jwt"
+	"github.com/stretchr/testify/assert"
+)
+
+// generate jwts for testing the expDate
+func generateDummyToken(expDate int64) string {
+	// Generate a new private key
+	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		log.Fatalf("Failed to generate private key: %s", err)
+	}
+
+	// Create the Claims
+	claims := &jwt.StandardClaims{
+		Issuer: "test",
+	}
+	if expDate != 0 {
+		claims = &jwt.StandardClaims{
+			ExpiresAt: expDate,
+			Issuer:    "test",
+		}
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodRS256, claims)
+	ss, err := token.SignedString(privateKey)
+	if err != nil {
+		log.Fatalf("Failed to sign token: %s", err)
+	}
+
+	return ss
+}
+
+func TestTokenExpiration(t *testing.T) {
+	// Token without exp claim
+	token := generateDummyToken(0)
+	err := CheckTokenExpiration(token)
+	assert.EqualError(t, err, "could not parse token, reason: no expiration date")
+
+	// Token with expired date
+	token = generateDummyToken(time.Now().Unix())
+	err = CheckTokenExpiration(token)
+	assert.EqualError(t, err, "the provided access token has expired, please renew it")
+
+	// Token with valid expiration
+	token = generateDummyToken(time.Now().Add(time.Hour * 72).Unix())
+	err = CheckTokenExpiration(token)
+	assert.NoError(t, err)
+}
+
+func TestGetBody(t *testing.T) {
+	// Mock server
+	mockResponse := `{"key": "value"}`
+	server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		rw.Write([]byte(mockResponse))
+	}))
+	defer server.Close()
+
+	// Test successful case
+	body, err := GetBody(server.URL, "mock_token")
+	assert.NoError(t, err)
+	assert.JSONEq(t, mockResponse, string(body))
+
+	// Test non-200 status code
+	serverError := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		rw.WriteHeader(http.StatusInternalServerError)
+	}))
+	defer serverError.Close()
+
+	body, err = GetBody(serverError.URL, "mock_token")
+	assert.Error(t, err)
+	assert.Nil(t, body)
+}
+
+func TestPostReq(t *testing.T) {
+	// Mock server
+	mockResponse := `{"key": "value"}` 
+	server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		assert.Equal(t, "application/json", req.Header.Get("Content-Type"))
+		assert.Equal(t, "Bearer mock_token", req.Header.Get("Authorization"))
+		rw.Write([]byte(mockResponse))
+	}))
+	defer server.Close()
+
+	// Test successful case
+	body, err := PostReq(server.URL, "mock_token", []byte(`{"name":"test"}`))
+	assert.NoError(t, err)
+	assert.JSONEq(t, mockResponse, string(body))
+
+	// Test non-200 status code
+	serverError := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		rw.WriteHeader(http.StatusInternalServerError)
+	}))
+	defer serverError.Close()
+
+	body, err = PostReq(serverError.URL, "mock_token", []byte(`{"name":"test"}`))
+	assert.Error(t, err)
+	assert.Nil(t, body)
+}
