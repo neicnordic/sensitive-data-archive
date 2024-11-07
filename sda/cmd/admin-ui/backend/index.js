057ce49c1457062560bd42dc1d6f7498010820fa
Annika <annika.sonnek@nbis.se>
2024-11-07 14:34:25 +0100

    clean up code, mock users, accessibility checks

--- sda/cmd/admin-ui/backend/index.js
+++ sda/cmd/admin-ui/backend/index.js
@@ -7,31 +7,31 @@ app.use(cors());
 
 const httpsAgent = new https.Agent({ rejectUnauthorized: false });
 
-//TODO Replace with the first test token you obtain from the tokens endpoint
+// See README on how to get the token
 function getToken() {
-    return 'token';
-
+    return 'token'; // Replace with your token
 }
 
-app.get('/api/files', async (req, res) => {
-    try {
-        const token = getToken();
-        if (!token)
-            return res.status(500).json({ error: 'Failed to obtain token' });
-        const apiResponse = await fetch('http://api:8090/files', {
-            headers: {
-                Authorization: `Bearer ${token}`,
-                'Content-Type': 'application/json'
-            },
-            agent: httpsAgent
-        });
+app.get('/api/files', async (request, response) => {
+  try {
+      const token = getToken();
+      console.log('Token:', token);
+      if (!token)
+        return response.status(500).json({ error: 'Failed to obtain token' });
+      const apiResponse = await fetch('http://api:8090/files', {
+        headers: {
+            Authorization: `Bearer ${token}`,
+            'Content-Type': 'application/json'
+        },
+        agent: httpsAgent
+      });
 
-        const responseData = await apiResponse.json();
-        res.json(responseData);
-    } catch (error) {
-        console.error('Error fetching data from API:', error);
-        res.status(500).json({ error: 'Failed to fetch data from API' });
-    }
+      const responseData = await apiResponse.json();
+      response.json(responseData);
+  } catch (error) {
+      console.error('Error fetching data from API:', error);
+      response.status(500).json({ error: 'Failed to fetch data from API' });
+  }
 });
 
 app.listen(3000, () => {
--- sda/cmd/admin-ui/frontend/static/script.js
+++ sda/cmd/admin-ui/frontend/static/script.js
@@ -1,30 +1,3 @@
-/* async function getToken() {
-  const url = "https://localhost:8080/tokens";
-
-  try {
-    const response = await fetch(url, {
-      method: 'GET', 
-      headers: {
-        'Access-Control-Allow-Origin': 'no-cors'
-      }
-    });
-
-    if (!response.ok) {
-      throw new Error(`Response status: ${response.status}`);
-    }
-
-    const data = await response.json();
-    const token = data[0]
-    console.log(data[0]);
-    return token
-
-  } catch (error) {
-    console.error(error);
-  }
-}
- */
-
-
 async function fetchDataWithToken() {
   try {
     const response = await fetch('http://localhost:3000/api/files', {
@@ -39,85 +12,96 @@ async function fetchDataWithToken() {
     }
 
     const data = await response.json();
-    console.log('Fetched data with token:', data);
-
-    // Parse the response data and update the table
-    const table = document.getElementById('filesTable');
-    data.forEach(item => { // note the reference to data rather than response
-      const row = table.insertRow();
-      const cell1 = row.insertCell(0);
-      const cell2 = row.insertCell(1);
-      const cell3 = row.insertCell(2);
-
-      switch (item.fileStatus.toLowerCase()) {
-        case 'uploaded':
-        case 'submitted':
-        case 'ingested':
-        case 'archived':
-        case 'verified':
-        case 'backed up':
-        case 'ready':
-          cell2.classList.add('text-success');
-          break;
-        case 'downloaded':
-          cell2.classList.add('text-primary');
-          break;
-        case 'error':
-          cell2.classList.add('text-danger');
-          break;
-        case 'disabled':
-          cell2.classList.add('text-muted');
-          break;
-        case 'enabled':
-          cell2.classList.add('text-info');
-          break;
-      }
-      cell1.innerText = item.createAt;
-      cell2.innerText = item.fileStatus;
-      cell3.innerText = item.inboxPath;
-    });
-
+    console.log('Data:', data);
+    populateFilesTable(data);
+    
   } catch (error) {
     console.error('Failed to fetch data with token:', error);
   }
 }
 
-document.getElementById('files-tab').addEventListener('click', function () {
-  fetchDataWithToken();
-});
+function populateFilesTable(data) {
+  const table = document.getElementById('filesTable');
+  data.forEach(item => {
+    const row = table.insertRow();
+    const cell1 = row.insertCell(0);
+    const cell2 = row.insertCell(1);
+    const cell3 = row.insertCell(2);
 
-
-// MATRIX RAIN ANIMATION
-var canvas = document.querySelector('canvas'),
-    ctx = canvas.getContext('2d');
-
-
-canvas.width = window.innerWidth;
-canvas.height = document.querySelector('.navbar').offsetHeight;
-
-var letters = '01001000 01100101 01101100 01101100 01101111 00100001 01001000 01100101 01101100 01101100 01101111 00100001 01001000 01100101 01101100 01101100 01101111 00100001';
-letters = letters.split('');
-
-var fontSize = 10,
-    columns = canvas.width / fontSize;
-
-var drops = [];
-for (var i = 0; i < columns; i++) {
-  drops[i] = 1;
+    switch (item.fileStatus.toLowerCase()) {
+      case 'uploaded':
+      case 'submitted':
+      case 'ingested':
+      case 'archived':
+      case 'verified':
+      case 'backed up':
+      case 'ready':
+        cell2.classList.add('text-success');
+        break;
+      case 'downloaded':
+        cell2.classList.add('text-primary');
+        break;
+      case 'error':
+        cell2.classList.add('text-danger');
+        break;
+      case 'disabled':
+        cell2.classList.add('text-muted');
+        break;
+      case 'enabled':
+        cell2.classList.add('text-info');
+        break;
+    }
+    cell1.innerText = item.createAt;
+    cell2.innerText = item.fileStatus;
+    cell3.innerText = item.inboxPath;
+  });
 }
 
-function draw() {
-  ctx.fillStyle = 'rgba(0, 0, 0, .1)';
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-  for (var i = 0; i < drops.length; i++) {
-    var text = letters[Math.floor(Math.random() * letters.length)];
-    ctx.fillStyle = '#0f0';
-    ctx.fillText(text, i * fontSize, drops[i] * fontSize);
-    drops[i]++;
-    if (drops[i] * fontSize > canvas.height && Math.random() > .95) {
-      drops[i] = 0;
+function populateUsersTable() {
+  const table = document.getElementById('usersTable');
+  const users = ['x@x.com', 'bird@bird.com','dinosaur@dino.com' ];
+
+  users.forEach(user => {
+    const row = table.insertRow();
+    const cell1 = row.insertCell(0);
+    cell1.innerText = user;
+  });
+}
+
+function matrixRainAnimation() {
+  let canvas = document.querySelector('canvas'),
+      ctx = canvas.getContext('2d');
+  
+  canvas.width = window.innerWidth;
+  canvas.height = document.querySelector('.navbar').offsetHeight;
+
+  let letters = '01001000 01100101 01101100 01101100 01101111 00100001 01001000 01100101 01101100 01101100 01101111 00100001 01001000 01100101 01101100 01101100 01101111 00100001';
+  letters = letters.split('');
+
+  let fontSize = 10,
+  columns = canvas.width / fontSize;
+
+  let drops = [];
+  for (let i = 0; i < columns; i++) {
+    drops[i] = 1;
+  }
+
+  function draw() {
+    ctx.fillStyle = 'rgba(0, 0, 0, .1)';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    for (let i = 0; i < drops.length; i++) {
+      let text = letters[Math.floor(Math.random() * letters.length)];
+      ctx.fillStyle = '#0f0';
+      ctx.fillText(text, i * fontSize, drops[i] * fontSize);
+      drops[i]++;
+      if (drops[i] * fontSize > canvas.height && Math.random() > .95) {
+        drops[i] = 0;
+      }
     }
   }
+  setInterval(draw, 100);
 }
 
-setInterval(draw, 100);
+matrixRainAnimation()
+fetchDataWithToken()
+populateUsersTable()
