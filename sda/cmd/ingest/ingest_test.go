797eaf0d2f2305531af06c806815ee7644150be4
Joakim Bygdell <joakim.bygdell@nbis.se>
2025-02-27 13:11:52 +0100

    [ingest_test] Set up suite for live testing

--- sda/cmd/ingest/ingest_test.go
+++ sda/cmd/ingest/ingest_test.go
@@ -1,66 +1,300 @@
 package main
 
 import (
+	"crypto/rand"
+	"database/sql"
+	"encoding/hex"
 	"fmt"
 	"io"
+	"net/http"
 	"os"
+	"path"
+	"path/filepath"
+	"runtime"
+	"strconv"
 	"testing"
+	"time"
 
+	log "github.com/sirupsen/logrus"
+
+	"github.com/google/uuid"
 	"github.com/neicnordic/crypt4gh/keys"
 	"github.com/neicnordic/crypt4gh/streaming"
+	"github.com/neicnordic/sensitive-data-archive/internal/broker"
 	"github.com/neicnordic/sensitive-data-archive/internal/config"
+	"github.com/neicnordic/sensitive-data-archive/internal/database"
 	"github.com/neicnordic/sensitive-data-archive/internal/helper"
+	"github.com/neicnordic/sensitive-data-archive/internal/schema"
+	"github.com/neicnordic/sensitive-data-archive/internal/storage"
+	"github.com/ory/dockertest/v3"
+	"github.com/ory/dockertest/v3/docker"
 	"github.com/spf13/viper"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
 )
 
-type TestSuite struct {
-	suite.Suite
+var DBport, MQport int
+var BrokerAPI string
+
+func TestMain(m *testing.M) {
+	if _, err := os.Stat("/.dockerenv"); err == nil {
+		m.Run()
+	}
+	_, b, _, _ := runtime.Caller(0)
+	rootDir := path.Join(path.Dir(b), "../../../")
+
+	// uses a sensible default on windows (tcp/http) and linux/osx (socket)
+	pool, err := dockertest.NewPool("")
+	if err != nil {
+		log.Fatalf("Could not construct pool: %s", err)
+	}
+
+	// uses pool to try to connect to Docker
+	err = pool.Client.Ping()
+	if err != nil {
+		log.Fatalf("Could not connect to Docker: %s", err)
+	}
+
+	// pulls an image, creates a container based on it and runs it
+	postgres, err := pool.RunWithOptions(&dockertest.RunOptions{
+		Repository: "postgres",
+		Tag:        "15.2-alpine3.17",
+		Env: []string{
+			"POSTGRES_PASSWORD=rootpasswd",
+			"POSTGRES_DB=sda",
+		},
+		Mounts: []string{
+			fmt.Sprintf("%s/postgresql/initdb.d:/docker-entrypoint-initdb.d", rootDir),
+		},
+	}, func(config *docker.HostConfig) {
+		// set AutoRemove to true so that stopped container goes away by itself
+		config.AutoRemove = true
+		config.RestartPolicy = docker.RestartPolicy{
+			Name: "no",
+		}
+	})
+	if err != nil {
+		log.Fatalf("Could not start resource: %s", err)
+	}
+
+	dbHostAndPort := postgres.GetHostPort("5432/tcp")
+	DBport, _ = strconv.Atoi(postgres.GetPort("5432/tcp"))
+	databaseURL := fmt.Sprintf("postgres://postgres:rootpasswd@%s/sda?sslmode=disable", dbHostAndPort)
+
+	pool.MaxWait = 120 * time.Second
+	if err = pool.Retry(func() error {
+		db, err := sql.Open("postgres", databaseURL)
+		if err != nil {
+			log.Println(err)
+
+			return err
+		}
+
+		query := "SELECT MAX(version) FROM sda.dbschema_version;"
+		var dbVersion int
+
+		return db.QueryRow(query).Scan(&dbVersion)
+	}); err != nil {
+		log.Fatalf("Could not connect to postgres: %s", err)
+	}
+
+	// pulls an image, creates a container based on it and runs it
+	rabbitmq, err := pool.RunWithOptions(&dockertest.RunOptions{
+		Repository: "ghcr.io/neicnordic/sensitive-data-archive",
+		Tag:        "v0.3.89-rabbitmq",
+	}, func(config *docker.HostConfig) {
+		// set AutoRemove to true so that stopped container goes away by itself
+		config.AutoRemove = true
+		config.RestartPolicy = docker.RestartPolicy{
+			Name: "no",
+		}
+	})
+	if err != nil {
+		if err := pool.Purge(postgres); err != nil {
+			log.Fatalf("Could not purge resource: %s", err)
+		}
+		log.Fatalf("Could not start resource: %s", err)
+	}
+
+	MQport, _ = strconv.Atoi(rabbitmq.GetPort("5672/tcp"))
+	BrokerAPI = rabbitmq.GetHostPort("15672/tcp")
+
+	client := http.Client{Timeout: 30 * time.Second}
+	req, err := http.NewRequest(http.MethodGet, "http://"+BrokerAPI+"/api/queues/sda/", http.NoBody)
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.SetBasicAuth("guest", "guest")
+
+	// exponential backoff-retry, because the application in the container might not be ready to accept connections yet
+	if err := pool.Retry(func() error {
+		res, err := client.Do(req)
+		if err != nil || res.StatusCode != 200 {
+			return err
+		}
+		res.Body.Close()
+
+		return nil
+	}); err != nil {
+		if err := pool.Purge(postgres); err != nil {
+			log.Fatalf("Could not purge resource: %s", err)
+		}
+		if err := pool.Purge(rabbitmq); err != nil {
+			log.Fatalf("Could not purge resource: %s", err)
+		}
+		log.Fatalf("Could not connect to rabbitmq: %s", err)
+	}
+
+	log.Println("starting tests")
+	code := m.Run()
+
+	log.Println("tests completed")
+	if err := pool.Purge(postgres); err != nil {
+		log.Fatalf("Could not purge resource: %s", err)
+	}
+	if err := pool.Purge(rabbitmq); err != nil {
+		log.Fatalf("Could not purge resource: %s", err)
+	}
+
+	os.Exit(code)
 }
 
-var pubKeyList = [][32]byte{}
-
-func TestConfigTestSuite(t *testing.T) {
+func TestIngestTestSuite(t *testing.T) {
 	suite.Run(t, new(TestSuite))
 }
 
-func (suite *TestSuite) SetupTest() {
-	viper.Set("log.level", "debug")
-	archive := suite.T().TempDir()
-	defer os.RemoveAll(archive)
-	viper.Set("archive.location", archive)
+type TestSuite struct {
+	suite.Suite
+	filePath   string
+	pubKeyList [][32]byte
+	ingest     Ingest
+}
 
+func (suite *TestSuite) SetupSuite() {
+	viper.Set("log.level", "debug")
 	tempDir := suite.T().TempDir()
 	keyFile1 := fmt.Sprintf("%s/c4gh1.key", tempDir)
 	keyFile2 := fmt.Sprintf("%s/c4gh2.key", tempDir)
 
 	publicKey, err := helper.CreatePrivateKeyFile(keyFile1, "test")
-	assert.NoError(suite.T(), err)
+	if err != nil {
+		suite.FailNow("Failed to create c4gh key")
+	}
 	// Add only the first public key to the list
-	pubKeyList = append(pubKeyList, publicKey)
+	suite.pubKeyList = append(suite.pubKeyList, publicKey)
 
 	_, err = helper.CreatePrivateKeyFile(keyFile2, "test")
-	assert.NoError(suite.T(), err)
+	if err != nil {
+		suite.FailNow("Failed to create c4gh key")
+	}
 
 	viper.Set("c4gh.privateKeys", []config.C4GHprivateKeyConf{
 		{FilePath: keyFile1, Passphrase: "test"},
 		{FilePath: keyFile2, Passphrase: "test"},
 	})
+	viper.Set("archive.type", "posix")
+	viper.Set("archive.location", "/tmp/")
+	viper.Set("broker.host", "localhost")
+	viper.Set("broker.port", MQport)
+	viper.Set("broker.user", "guest")
+	viper.Set("broker.password", "guest")
+	viper.Set("broker.queue", "ingest")
+	viper.Set("broker.routingkey", "verify")
+	viper.Set("broker.vhost", "sda")
+	viper.Set("db.host", "localhost")
+	viper.Set("db.port", DBport)
+	viper.Set("db.user", "postgres")
+	viper.Set("db.password", "rootpasswd")
+	viper.Set("db.database", "sda")
+	viper.Set("db.sslMode", "disable")
+	viper.Set("inbox.type", "posix")
+	viper.Set("inbox.location", "/tmp/")
+	viper.Set("schema.path", "../../schemas/isolated/")
 
-	viper.Set("broker.host", "test")
-	viper.Set("broker.port", 123)
-	viper.Set("broker.user", "test")
-	viper.Set("broker.password", "test")
-	viper.Set("broker.queue", "test")
-	viper.Set("broker.routingkey", "test")
-	viper.Set("db.host", "test")
-	viper.Set("db.port", 123)
-	viper.Set("db.user", "test")
-	viper.Set("db.password", "test")
-	viper.Set("db.database", "test")
+	suite.ingest.Conf, err = config.NewConfig("ingest")
+	if err != nil {
+		suite.FailNowf("failed to init config: %s", err.Error())
+	}
+	suite.ingest.DB, err = database.NewSDAdb(suite.ingest.Conf.Database)
+	if err != nil {
+		suite.FailNowf("failed to setup database connection: %s", err.Error())
+	}
+	suite.ingest.MQ, err = broker.NewMQ(suite.ingest.Conf.Broker)
+	if err != nil {
+		suite.FailNowf("failed to setup rabbitMQ connection: %s", err.Error())
+	}
+	suite.ingest.ArchiveKeyList, err = config.GetC4GHprivateKeys()
+	if err != nil {
+		suite.FailNow("no private keys configured")
+	}
+
+	if err := suite.ingest.DB.AddKeyHash(hex.EncodeToString(publicKey[:]), "the test key"); err != nil {
+		suite.FailNow("failed to register the public key")
+	}
 }
 
+func (suite *TestSuite) TearDownSuite() {
+	_ = os.RemoveAll(suite.ingest.Conf.Archive.Posix.Location)
+	_ = os.RemoveAll(suite.ingest.Conf.Inbox.Posix.Location)
+}
+
+func (suite *TestSuite) SetupTest() {
+	var err error
+	suite.ingest.Conf.Archive.Posix.Location, err = os.MkdirTemp("", "archive")
+	if err != nil {
+		suite.FailNow("failed to create temp folder")
+	}
+
+	suite.ingest.Conf.Inbox.Posix.Location, err = os.MkdirTemp("", "inbox")
+	if err != nil {
+		suite.FailNow("failed to create temp folder")
+	}
+
+	f, err := os.CreateTemp(suite.ingest.Conf.Inbox.Posix.Location, "")
+	if err != nil {
+		suite.FailNow("failed to create test file")
+	}
+	defer f.Close()
+
+	_, err = io.Copy(f, io.LimitReader(rand.Reader, 10*1024*1024))
+	if err != nil {
+		suite.FailNow("failed to write data to test file")
+	}
+
+	outFileName := f.Name() + ".c4gh"
+	outFile, err := os.Create(outFileName)
+	if err != nil {
+		suite.FailNow("failed to create encrypted test file")
+	}
+	defer outFile.Close()
+
+	_, privateKey, err := keys.GenerateKeyPair()
+	if err != nil {
+		suite.FailNow("failed to create private c4gh key")
+	}
+
+	crypt4GHWriter, err := streaming.NewCrypt4GHWriter(outFile, privateKey, suite.pubKeyList, nil)
+	if err != nil {
+		suite.FailNow("failed to create c4gh writer")
+	}
+
+	_, err = io.Copy(crypt4GHWriter, io.LimitReader(rand.Reader, 10*1024*1024))
+	if err != nil {
+		suite.FailNow("failed to write data to encrypted test file")
+	}
+	crypt4GHWriter.Close()
+
+	suite.filePath = filepath.Base(outFileName)
+
+	suite.ingest.Archive, err = storage.NewBackend(suite.ingest.Conf.Archive)
+	if err != nil {
+		suite.FailNow("failed to setup archive backend")
+	}
+	suite.ingest.Inbox, err = storage.NewBackend(suite.ingest.Conf.Inbox)
+	if err != nil {
+		suite.FailNow("failed to setup inbox backend")
+	}
+}
 func (suite *TestSuite) TestTryDecrypt_wrongFile() {
 	tempDir := suite.T().TempDir()
 	err := os.WriteFile(fmt.Sprintf("%s/dummy.file", tempDir), []byte("hello\ngo\n"), 0600)
@@ -80,7 +314,6 @@ func (suite *TestSuite) TestTryDecrypt_wrongFile() {
 	assert.Nil(suite.T(), header)
 	assert.EqualError(suite.T(), err, "not a Crypt4GH file")
 }
-
 func (suite *TestSuite) TestTryDecrypt() {
 	_, signingKey, err := keys.GenerateKeyPair()
 	assert.NoError(suite.T(), err)
@@ -96,7 +329,7 @@ func (suite *TestSuite) TestTryDecrypt() {
 	encryptedFile, err := os.CreateTemp(tempDir, "encryptedFile-")
 	assert.NoError(suite.T(), err)
 
-	crypt4GHWriter, err := streaming.NewCrypt4GHWriter(encryptedFile, signingKey, pubKeyList, nil)
+	crypt4GHWriter, err := streaming.NewCrypt4GHWriter(encryptedFile, signingKey, suite.pubKeyList, nil)
 	assert.NoError(suite.T(), err)
 
 	_, err = io.Copy(crypt4GHWriter, unencryptedFile)
