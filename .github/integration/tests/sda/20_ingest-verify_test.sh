aa0969e277cbc11f26767e2680f7a83144884625
Joakim Bygdell <joakim.bygdell@nbis.se>
2025-08-13 15:59:02 +0200

    [integration tests] old style file paths should be ok.

--- .github/integration/tests/sda/20_ingest-verify_test.sh
+++ .github/integration/tests/sda/20_ingest-verify_test.sh
@@ -32,6 +32,10 @@ for file in NA12878.bam NA12878_20k_b37.bam NA12878.bai NA12878_20k_b37.bai; do
             '$ARGS.named|to_entries|map(with_entries(select(.key=="key").key="type"))'
     )
 
+    if [ "$file" = "NA12878.bam" ]; then
+        file="test_dummy.org/NA12878.bam"
+    fi
+
     ingest_payload=$(
         jq -r -c -n \
             --arg type ingest \
