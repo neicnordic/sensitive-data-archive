b37b82780df8942c269e85848508e9dcb24e8dbe
kostas-kou <kkoumpouras@gmail.com>
2025-08-22 11:18:56 +0200

    Create function in api for accepting file id as parameter

--- sda/cmd/api/api.go
+++ sda/cmd/api/api.go
@@ -320,9 +320,22 @@ func getFiles(c *gin.Context) {
 // ingestFile function sends the ingest message
 // to the broker
 func ingestFile(c *gin.Context) {
-	ingest, corrID, err := msgInfoFilePath(c)
-	if err != nil {
-		return
+	var (
+		ingest schema.IngestionTrigger
+		corrID string
+		err    error
+	)
+	fileID := c.Query("fileid")
+	if fileID == "" {
+		ingest, corrID, err = ingestMsgFilePath(c)
+		if err != nil {
+			return
+		}
+	} else {
+		ingest, corrID, err = ingestMsgFileID(c, fileID)
+		if err != nil {
+			return
+		}
 	}
 
 	marshaledMsg, _ := json.Marshal(&ingest)
@@ -372,6 +385,38 @@ func ingestMsgFilePath(c *gin.Context) (schema.IngestionTrigger, string, error) 
 	}
 	// Add type in message payload
 	ingest.Type = "ingest"
+
+	return ingest, corrID, nil
+}
+
+// ingestMsgFileID constructs an ingestion trigger message using a file UUID provided as a query parameter.
+// It checks that no JSON payload is present in the request; if both are provided, it returns a 400 Bad Request.
+// The function retrieves the user and file path from the database using the file UUID.
+// If the file UUID is not found, it responds with a 404 Not Found error.
+// Returns the constructed ingestion trigger struct, the correlation ID (file UUID), and an error if any occurred.
+func ingestMsgFileID(c *gin.Context, fileUUID string) (schema.IngestionTrigger, string, error) {
+	var ingest schema.IngestionTrigger
+	// Check if payload is provided as well
+	if c.Request.ContentLength > 0 {
+		c.AbortWithStatusJSON(http.StatusBadRequest, "Both file ID parameter and payload provided. Choose one")
+
+		return schema.IngestionTrigger{}, "", errors.New("add either parameter or payload, not both")
+	}
+	// Get the user and the inbox filepath
+	ingestInfo, err := Conf.API.DB.GetUserAndPathFromUUID(fileUUID)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusNotFound, "file ID not found")
+
+		return schema.IngestionTrigger{}, "", err
+	}
+
+	// Information needed for the ingest message
+	ingest.Type = "ingest"
+	ingest.User = ingestInfo.User
+	ingest.FilePath = ingestInfo.InboxPath
+	// For BP the file UUID and the correlation ID are the same.
+	// TODO: If in GDI they are not the same then change the line below
+	corrID := fileUUID
 
 	return ingest, corrID, nil
 }
