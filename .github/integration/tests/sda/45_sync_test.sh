be9bfdaeeaacdef2d11e33ba204363d6eeafd8e6
Alex Aperis <aperisalex@gmail.com>
2025-09-04 18:52:48 +0200

    add rotatekey integration tests

--- .github/integration/tests/sda/45_sync_test.sh
+++ .github/integration/tests/sda/45_sync_test.sh
@@ -11,7 +11,7 @@ fi
 # check bucket for synced files
 for file in NA12878.bai NA12878_20k_b37.bai; do
     RETRY_TIMES=0
-    until [ "$(s3cmd -c direct ls s3://sync/test_dummy.org/"$file")" != "" ]; do
+    until [ "$(s3cmd -c direct ls s3://sync/"$file")" != "" ]; do
         RETRY_TIMES=$((RETRY_TIMES + 1))
         if [ "$RETRY_TIMES" -eq 30 ]; then
             echo "::error::Time out while waiting for files to be synced"
