01e6db18daf10a3156c616f7c1cdcee12c43b1eb
Panos Chatzopoulos <panos.chatzopoulos@nbis.se>
2025-10-28 14:03:15 +0100

    [sda-admin] add rotate key for files of a dataset

--- sda-admin/dataset/dataset.go
+++ sda-admin/dataset/dataset.go
@@ -15,6 +15,11 @@ type RequestBodyDataset struct {
 	User         string   `json:"user"`
 }
 
+type DatasetFileIDs struct {
+	FileID      string `json:"fileID"`
+	AccessionID string `json:"accessionID"`
+}
+
 // Create creates a dataset from a list of accession IDs and a dataset ID.
 func Create(apiURI, token, datasetID, username string, accessionIDs []string) error {
 	parsedURL, err := url.Parse(apiURI)
@@ -53,6 +58,47 @@ func Release(apiURI, token, datasetID string) error {
 	_, err = helpers.PostRequest(parsedURL.String(), token, nil)
 	if err != nil {
 		return err
+	}
+
+	return nil
+}
+
+// RotateKey rotates the encryption key for all files in a dataset
+func RotateKey(apiURI, token, datasetID string) error {
+	// First, get all file IDs in the dataset
+	filesURL, err := url.Parse(apiURI)
+	if err != nil {
+		return err
+	}
+	filesURL.Path = path.Join(filesURL.Path, "dataset", datasetID, "fileids")
+
+	response, err := helpers.GetResponseBody(filesURL.String(), token)
+	if err != nil {
+		return fmt.Errorf("failed to get dataset file IDs, reason: %v", err)
+	}
+
+	var datasetFiles []DatasetFileIDs
+	err = json.Unmarshal(response, &datasetFiles)
+	if err != nil {
+		return fmt.Errorf("failed to unmarshal dataset file IDs response, reason: %v", err)
+	}
+
+	if len(datasetFiles) == 0 {
+		return fmt.Errorf("no files found for dataset %s", datasetID)
+	}
+
+	// Send rotation request for each file using the internal FileID
+	for _, file := range datasetFiles {
+		rotateURL, err := url.Parse(apiURI)
+		if err != nil {
+			return fmt.Errorf("failed to parse API URI for file %s, reason: %v", file.FileID, err)
+		}
+		rotateURL.Path = path.Join(rotateURL.Path, "file", "rotatekey", file.FileID)
+
+		_, err = helpers.PostRequest(rotateURL.String(), token, nil)
+		if err != nil {
+			return fmt.Errorf("failed to rotate key for file %s (accession: %s), reason: %v", file.FileID, file.AccessionID, err)
+		}
 	}
 
 	return nil
