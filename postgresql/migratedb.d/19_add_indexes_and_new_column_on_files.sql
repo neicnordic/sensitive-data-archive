9eb80cb1d2b8e8d8c423c2f70a18db8da9d80b72
Karl Grönberg <karl.gronberg@nbis.se>
2025-11-19 16:00:15 +0100

    feat(postgresql): update init of db to add new indexes and colum changes on files and file_event_log tables, add migration steps to reflect indexes and columns changes, and deprecation of file_event_log.correlation_id with data migration

---
+++ postgresql/migratedb.d/19_add_indexes_and_new_column_on_files.sql
@@ -0,0 +1,27 @@
+
+DO
+$$
+DECLARE
+-- The version we know how to do migration from, at the end of a successful migration
+-- we will no longer be at this version.
+  sourcever INTEGER := 18;
+  changes VARCHAR := 'Create new indexes on files and file_event_log tables, and new generated column submission_file_root_dir on files table';
+BEGIN
+  IF (select max(version) from sda.dbschema_version) = sourcever then
+    RAISE NOTICE 'Doing migration from schema version % to %', sourcever, sourcever+1;
+    RAISE NOTICE 'Changes: %', changes;
+    INSERT INTO sda.dbschema_version VALUES(sourcever+1, now(), changes);
+
+
+    ALTER TABLE sda.files
+        ADD COLUMN submission_file_root_dir TEXT GENERATED ALWAYS AS (split_part(submission_file_path, '/', 1)) STORED;
+
+
+    CREATE INDEX files_submission_user_submission_file_root_dir_idx
+        ON sda.files(submission_user, submission_file_root_dir);
+
+  ELSE
+    RAISE NOTICE 'Schema migration from % to % does not apply now, skipping', sourcever, sourcever+1;
+  END IF;
+END
+$$
