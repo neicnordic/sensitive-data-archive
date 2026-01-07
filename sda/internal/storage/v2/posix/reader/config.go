f0f53cc2a82ea745630bab7e07afbcbbc02eff1b
Karl Grönberg <karl.gronberg@nbis.se>
2026-01-07 15:30:14 +0100

    feat(storage-v2): update posix reader config to follow same structre as posix writer, but only with path as only required config for reader

--- sda/internal/storage/v2/posix/reader/config.go
+++ sda/internal/storage/v2/posix/reader/config.go
@@ -4,18 +4,34 @@ import (
 	"errors"
 	"strings"
 
+	"github.com/go-viper/mapstructure/v2"
 	"github.com/spf13/viper"
 )
 
-func loadConfig(backendName string) ([]string, error) {
-	// TODO ideally register these as flags so it could be included in --help, etc for easier usability
-	endpoints := viper.GetStringSlice("storage.posix." + backendName)
+type endpointConfig struct {
+	Path string `mapstructure:"path"`
+}
 
-	for _, e := range endpoints {
-		if !strings.HasPrefix(e, "/") {
+func loadConfig(backendName string) ([]*endpointConfig, error) {
+	var endpointConf []*endpointConfig
+
+	// TODO ideally register these as flags so it could be included in --help, etc for easier usability
+	if err := viper.UnmarshalKey(
+		"storage.posix."+backendName,
+		&endpointConf,
+		func(config *mapstructure.DecoderConfig) {
+			config.WeaklyTypedInput = true
+			config.ZeroFields = true
+		},
+	); err != nil {
+		return nil, err
+	}
+
+	for _, e := range endpointConf {
+		if !strings.HasPrefix(e.Path, "/") {
 			return nil, errors.New("posix paths must be absolute")
 		}
 	}
 
-	return endpoints, nil
+	return endpointConf, nil
 }
