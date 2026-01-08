eefbdf5dcd7cc3cdcef6578592757b0c7a2a5daf
Karl Grönberg <karl.gronberg@nbis.se>
2026-01-08 09:59:01 +0100

    feat(postgres): remove deprecated functions from 02_functions, which were removed in migration 21 step, update new 22 migration step to create register_file func as expected

--- postgresql/migratedb.d/22_expand_files_table_with_storage_locations.sql
+++ postgresql/migratedb.d/22_expand_files_table_with_storage_locations.sql
@@ -13,46 +13,36 @@ BEGIN
     INSERT INTO sda.dbschema_version VALUES(sourcever+1, now(), changes);
 
     ALTER TABLE sda.files
-    ADD COLUMN archive_location TEXT,
-    ADD COLUMN backup_location TEXT,
-    ADD COLUMN submission_location TEXT;
+        ADD COLUMN archive_location TEXT,
+        ADD COLUMN backup_location TEXT,
+        ADD COLUMN submission_location TEXT;
 
     DROP FUNCTION IF EXISTS sda.register_file;
-    CREATE FUNCTION sda.register_file(submission_location TEXT, submission_file_path TEXT, submission_user TEXT)
-    RETURNS TEXT AS $register_file$
+    CREATE FUNCTION sda.register_file(file_id TEXT, submission_location TEXT, submission_file_path TEXT, submission_user TEXT)
+        RETURNS TEXT AS $register_file$
     DECLARE
-        file_ext TEXT;
-        file_uuid UUID;
+    file_uuid UUID;
     BEGIN
-        INSERT INTO sda.files( submission_location, submission_file_path, submission_user, encryption_method )
-        VALUES( submission_location, submission_file_path, submission_user, 'CRYPT4GH' )
-            ON CONFLICT ON CONSTRAINT unique_ingested
-            DO UPDATE SET
-              submission_location = EXCLUDED.submission_location,
-              submission_file_path = EXCLUDED.submission_file_path,
-              submission_user = EXCLUDED.submission_user,
-              encryption_method = EXCLUDED.encryption_method
-            RETURNING id INTO file_uuid;
+        -- Upsert file information. we're not interested in restarted uploads so old
+        -- overwritten files that haven't been ingested are updated instead of
+        -- inserting a new row.
+    INSERT INTO sda.files( id, submission_location, submission_file_path, submission_user, encryption_method )
+    VALUES(  COALESCE(CAST(NULLIF(file_id, '') AS UUID), gen_random_uuid()), submission_location, submission_file_path, submission_user, 'CRYPT4GH' )
+        ON CONFLICT ON CONSTRAINT unique_ingested
+        DO UPDATE SET submission_location = EXCLUDED.submission_location,
+               submission_file_path = EXCLUDED.submission_file_path,
+               submission_user = EXCLUDED.submission_user,
+               encryption_method = EXCLUDED.encryption_method
+               RETURNING id INTO file_uuid;
 
-        INSERT INTO sda.file_event_log( file_id, event, user_id)
-        VALUES (file_uuid, 'registered', submission_user);
+    -- We add a new event for every registration though, as this might help for
+    -- debugging.
+    INSERT INTO sda.file_event_log( file_id, event, user_id )
+    VALUES (file_uuid, 'registered', submission_user);
 
-        RETURN file_uuid;
+    RETURN file_uuid;
     END;
     $register_file$ LANGUAGE plpgsql;
-
-    DROP FUNCTION IF EXISTS sda.set_archived;
-    CREATE FUNCTION sda.set_archived(file_uuid UUID, archive_loc TEXT, file_path TEXT, file_size BIGINT, inbox_checksum_value TEXT, inbox_checksum_type TEXT)
-    RETURNS void AS $set_archived$
-    BEGIN
-        UPDATE sda.files SET archive_location = archive_loc, archive_file_path = file_path, archive_file_size = file_size WHERE id = file_uuid;
-
-        INSERT INTO sda.checksums(file_id, checksum, type, source)
-        VALUES(file_uuid, inbox_checksum_value, upper(inbox_checksum_type)::sda.checksum_algorithm, upper('UPLOADED')::sda.checksum_source);
-
-        INSERT INTO sda.file_event_log(file_id, event) VALUES(file_uuid, 'archived');
-    END;
-    $set_archived$ LANGUAGE plpgsql;
 
   ELSE
     RAISE NOTICE 'Schema migration from % to % does not apply now, skipping', sourcever, sourcever+1;
