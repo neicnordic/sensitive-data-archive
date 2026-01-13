c8736c486b362ea35abbf957e2a914653743fa1e
Karl Grönberg <karl.gronberg@nbis.se>
2026-01-13 13:40:42 +0100

    feat(s3inbox): update required db version check to v23 with new location columns

--- sda/cmd/s3inbox/s3inbox.go
+++ sda/cmd/s3inbox/s3inbox.go
@@ -51,8 +51,8 @@ func main() {
 		sigc <- syscall.SIGINT
 		panic(err)
 	}
-	if sdaDB.Version < 21 {
-		log.Error("database schema v21 is required")
+	if sdaDB.Version < 23 {
+		log.Error("database schema v23 is required")
 		sigc <- syscall.SIGINT
 		panic(err)
 	}
