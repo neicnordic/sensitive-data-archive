6500703512bb677397b2a1eacec92aabd8d9c94a
Karl Grönberg <karl.gronberg@nbis.se>
2026-01-13 15:04:44 +0100

    feat(sync): fix database unit test, some linting issues, and update README.md to now mention expected storage config

--- sda/cmd/sync/sync.go
+++ sda/cmd/sync/sync.go
@@ -40,20 +40,26 @@ func main() {
 	forever := make(chan bool)
 	conf, err = config.NewConfig("sync")
 	if err != nil {
-		log.Fatal(err)
+		log.Error(err.Error())
+
+		return
 	}
 	mq, err := broker.NewMQ(conf.Broker)
 	if err != nil {
-		log.Fatal(err)
+		log.Error(err.Error())
+
+		return
 	}
 	db, err = database.NewSDAdb(conf.Database)
 	if err != nil {
-		log.Fatal(err)
+		log.Error(err.Error())
+
+		return
 	}
 	if db.Version < 23 {
 		log.Error("database schema v23 is required")
-		db.Close()
-		panic(err)
+
+		return
 	}
 
 	lb, err := locationbroker.NewLocationBroker(db)
@@ -62,16 +68,22 @@ func main() {
 	}
 	syncWriter, err = storage.NewWriter(ctx, "sync", lb)
 	if err != nil {
-		log.Fatal(err)
+		log.Error(err.Error())
+
+		return
 	}
 	archiveReader, err = storage.NewReader(ctx, "archive")
 	if err != nil {
-		log.Fatal(err)
+		log.Error(err.Error())
+
+		return
 	}
 
 	key, err = config.GetC4GHKey()
 	if err != nil {
-		log.Fatal(err)
+		log.Error(err.Error())
+
+		return
 	}
 
 	defer mq.Channel.Close()
