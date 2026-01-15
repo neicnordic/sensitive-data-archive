510832671030b486859f94a6ffcc9a7373392836
Karl Grönberg <karl.gronberg@nbis.se>
2026-01-15 14:43:26 +0100

    feat(verify): fix scanning in GetArchiveLocation incase archive_location is null, add check if archive location is set and add event log error if its not known

--- sda/internal/database/db_functions.go
+++ sda/internal/database/db_functions.go
@@ -918,13 +918,16 @@ func (dbs *SDAdb) GetArchiveLocation(fileID string) (string, error) {
 	db := dbs.DB
 	const getFileID = "SELECT archive_location from sda.files WHERE id = $1;"
 
-	var archiveLocation string
-	err := db.QueryRow(getFileID, fileID).Scan(&archiveLocation)
-	if err != nil {
+	var archiveLocation sql.NullString
+
+	if err := db.QueryRow(getFileID, fileID).Scan(&archiveLocation); err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return "", nil
+		}
 		return "", err
 	}
 
-	return archiveLocation, nil
+	return archiveLocation.String, nil
 }
 
 // SetSubmissionFileSize sets the submission file size for a file
