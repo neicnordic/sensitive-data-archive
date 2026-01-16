d08927bbe67dbd0c1b8b4e60d9e3b3f9a8653f5c
Karl Grönberg <karl.gronberg@nbis.se>
2026-01-16 13:12:15 +0100

    feat(verify): update db version check to v23 after rebase

--- sda/cmd/verify/verify.go
+++ sda/cmd/verify/verify.go
@@ -45,8 +45,8 @@ func main() {
 
 		return
 	}
-	if db.Version < 22 {
-		log.Error("database schema v22 is required")
+	if db.Version < 23 {
+		log.Error("database schema v23 is required")
 
 		return
 	}
