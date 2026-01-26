30cbcda9ed4da6e6d45cbfb0ab01a74d9b1cff6b
Karl Grönberg <karl.gronberg@nbis.se>
2026-01-26 11:47:12 +0100

    feat(s3inbox): update main shutdown and error logic

--- sda/cmd/s3inbox/s3inbox.go
+++ sda/cmd/s3inbox/s3inbox.go
@@ -25,73 +25,68 @@ import (
 )
 
 func main() {
-	sigc := make(chan os.Signal, 5)
-	signal.Notify(sigc, os.Interrupt, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
-
-	// Create a function to handle panic and exit gracefully
-	defer func() {
-		if err := recover(); err != nil {
-			log.Errorf("Could not recover from %v", err)
-			log.Fatal("Could not recover, exiting")
-		}
-	}()
-
+	if err := run(); err != nil {
+		log.Fatal(err)
+	}
+}
+func run() error {
 	conf, err := config.NewConfig("s3inbox")
 	if err != nil {
-		log.Fatalf("failed to config due to: %v", err)
+		return fmt.Errorf("failed to load config due to: %v", err)
 	}
 
 	tlsProxy, err := config.TLSConfigProxy(conf)
 	if err != nil {
-		log.Fatalf("failed to setup tls config due to: %v", err)
+		return fmt.Errorf("failed to setup tls config due to: %v", err)
 	}
 
 	sdaDB, err := database.NewSDAdb(conf.Database)
 	if err != nil {
-		log.Fatalf("failed to init new db due to: %v", err)
+		return fmt.Errorf("failed to initialize sda db due to: %v", err)
 	}
+	defer sdaDB.Close()
 	if sdaDB.Version < 23 {
-		log.Fatalf("database schema v23 is required")
+		return errors.New("database schema v23 is required")
 	}
 
 	log.Debugf("Connected to sda-db (v%v)", sdaDB.Version)
 	log.Println(conf.S3Inbox.Endpoint + "/BUCKET == " + conf.S3Inbox.Bucket)
-	s3, err := newS3Client(conf.S3Inbox)
+	s3Client, err := newS3Client(conf.S3Inbox)
 	if err != nil {
-		log.Fatalf("failed to init new S3 client due to: %v", err)
+		return fmt.Errorf("failed to initialize new S3 client due to: %v", err)
 	}
 
-	err = checkS3Bucket(conf.S3Inbox.Bucket, s3)
-	if err != nil {
-		log.Fatalf("failed to check if inbox bucket exists due to: %v", err)
+	if err = checkS3Bucket(conf.S3Inbox.Bucket, s3Client); err != nil {
+		return fmt.Errorf("failed to check if inbox bucket exists due to: %v", err)
 	}
 
-	messenger, err := broker.NewMQ(conf.Broker)
+	mqBroker, err := broker.NewMQ(conf.Broker)
 	if err != nil {
-		log.Fatalf("failed to init broker due to: %v", err)
+		return fmt.Errorf("failed to initialize broker due to: %v", err)
 	}
-
-	go func() {
-		<-sigc
-		sdaDB.Close()
-		messenger.Channel.Close()
-		messenger.Connection.Close()
-		os.Exit(1)
+	defer func() {
+		if err := mqBroker.Channel.Close(); err != nil {
+			log.Errorf("failed to close mq broker channel due to: %v", err)
+		}
+		if err := mqBroker.Connection.Close(); err != nil {
+			log.Errorf("failed to close mq broker connection due to: %v", err)
+		}
 	}()
+
 	auth := userauth.NewValidateFromToken(jwk.NewSet())
 	// Load keys for JWT verification
 	if conf.Server.Jwtpubkeyurl != "" {
 		if err := auth.FetchJwtPubKeyURL(conf.Server.Jwtpubkeyurl); err != nil {
-			log.Panicf("Error while getting key %s: %v", conf.Server.Jwtpubkeyurl, err)
+			return fmt.Errorf("failed to read jwt pub key from url: %s, due to %v", conf.Server.Jwtpubkeyurl, err)
 		}
 	}
 	if conf.Server.Jwtpubkeypath != "" {
 		if err := auth.ReadJwtPubKeyPath(conf.Server.Jwtpubkeypath); err != nil {
-			log.Panicf("Error while getting key %s: %v", conf.Server.Jwtpubkeypath, err)
+			return fmt.Errorf("failed to read jwt pub key from path: %s, due to %v", conf.Server.Jwtpubkeypath, err)
 		}
 	}
 	router := mux.NewRouter()
-	proxy := NewProxy(conf.S3Inbox, auth, messenger, sdaDB, tlsProxy)
+	proxy := NewProxy(conf.S3Inbox, auth, mqBroker, sdaDB, tlsProxy)
 	router.HandleFunc("/", proxy.CheckHealth).Methods("HEAD")
 	router.HandleFunc("/health", proxy.CheckHealth)
 	router.PathPrefix("/").Handler(proxy)
@@ -105,15 +100,33 @@ func main() {
 		Handler:           router,
 	}
 
-	if conf.Server.Cert != "" && conf.Server.Key != "" {
-		if err := server.ListenAndServeTLS(conf.Server.Cert, conf.Server.Key); err != nil {
-			panic(err)
+	serverErr := make(chan error, 1)
+	go func() {
+		if conf.Server.Cert != "" && conf.Server.Key != "" {
+			if err := server.ListenAndServeTLS(conf.Server.Cert, conf.Server.Key); err != nil {
+				serverErr <- fmt.Errorf("failed to start https server, due to: %v", err)
+			}
+		} else {
+			if err := server.ListenAndServe(); err != nil {
+				serverErr <- fmt.Errorf("failed to start http server, due to: %v", err)
+			}
 		}
-	} else {
-		if err := server.ListenAndServe(); err != nil {
-			panic(err)
-		}
+	}()
+
+	sigc := make(chan os.Signal, 5)
+	signal.Notify(sigc, os.Interrupt, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+
+	select {
+	case <-sigc:
+	case err := <-serverErr:
+		return err
 	}
+
+	if err := server.Shutdown(context.Background()); err != nil {
+		log.Errorf("failed to close http/https server due to: %v", err)
+	}
+
+	return nil
 }
 
 func checkS3Bucket(bucket string, s3Client *s3.Client) error {
