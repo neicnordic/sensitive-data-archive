7a0bed266e717f33deb468660d79036c096ae287
Karl Grönberg <karl.gronberg@nbis.se>
2026-01-28 15:49:54 +0100

    feat(database): add CancelFile, and IsFileInDataset to the database interface, add unit test for those, fix issue in IsFileInDataset with archive_file_path isnt nullable

--- sda/internal/database/database_interface.go
+++ sda/internal/database/database_interface.go
@@ -6,4 +6,6 @@ type Database interface {
 	GetSizeAndObjectCountOfLocation(ctx context.Context, location string) (uint64, uint64, error)
 
 	GetSubmissionLocation(ctx context.Context, fileID string) (string, error)
+	CancelFile(ctx context.Context, fileID string) error
+	IsFileInDataset(ctx context.Context, fileID string) (bool, error)
 }
