9e7e2804a850b76f5a3d01d6de19006fe3d7236f
Karl Grönberg <karl.gronberg@nbis.se>
2026-02-02 16:05:45 +0100

    feat(download): update integration tests to work with sda-download using storage v2, use local built images in integration tests not hard coded versions, create db download user, use sda db instead of lega, etc

--- sda-download/.github/integration/tests/common/30_check_db.sh
+++ sda-download/.github/integration/tests/common/30_check_db.sh
@@ -7,15 +7,15 @@ chmod 600 certs/client-key.pem
 
 docker run --rm --name client --network dev_utils_default -v "$PWD/certs:/certs" \
 	-e PGSSLCERT=/certs/client.pem -e PGSSLKEY=/certs/client-key.pem -e PGSSLROOTCERT=/certs/ca.pem \
-	neicnordic/pg-client:latest postgresql://postgres:rootpassword@db:5432/lega \
+	neicnordic/pg-client:latest postgresql://postgres:rootpassword@db:5432/sda \
 	-t -c "SELECT * from sda.file_dataset"
 
 docker run --rm --name client --network dev_utils_default -v "$PWD/certs:/certs" \
 	-e PGSSLCERT=/certs/client.pem -e PGSSLKEY=/certs/client-key.pem -e PGSSLROOTCERT=/certs/ca.pem \
-	neicnordic/pg-client:latest postgresql://postgres:rootpassword@db:5432/lega \
+	neicnordic/pg-client:latest postgresql://postgres:rootpassword@db:5432/sda \
 	-t -c "SELECT * from sda.file_event_log"
 
 docker run --rm --name client --network dev_utils_default -v "$PWD/certs:/certs" \
 	-e PGSSLCERT=/certs/client.pem -e PGSSLKEY=/certs/client-key.pem -e PGSSLROOTCERT=/certs/ca.pem \
-	neicnordic/pg-client:latest postgresql://postgres:rootpassword@db:5432/lega \
+	neicnordic/pg-client:latest postgresql://postgres:rootpassword@db:5432/sda \
 	-t -c "SELECT id, stable_id, archive_file_path FROM sda.files ORDER BY id DESC"
