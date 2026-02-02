208f96c6717627ce40df447c420bc1029efa2050
Karl Grönberg <karl.gronberg@nbis.se>
2026-02-02 16:05:45 +0100

    feat(download): update integration tests to work with sda-download using storage v2, use local built images in integration tests not hard coded versions, create db download user, use sda db instead of lega, etc

---
+++ sda-download/.github/integration/setup/posix/24_posix_update_db_entry.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+docker run --rm --name client --network dev_utils_default \
+    -v "dev_utils_certs:/certs" \
+    -e PGSSLCERT=/certs/client.pem \
+    -e PGSSLKEY=/certs/client-key.pem \
+    -e PGSSLROOTCERT=/certs/ca.pem \
+    neicnordic/pg-client:latest \
+    postgresql://postgres:rootpassword@db:5432/sda \
+    -t -q -c "UPDATE sda.files SET archive_location = '/tmp';"
