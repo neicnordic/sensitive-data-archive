8a6dacc4c51b4b762a0a3f933c4facf5f54cea3a
Joshua Baskaran <joshbaskaran@gmail.com>
2026-02-09 20:02:11 +0100

    feat: new custom exception for JSON schema validation errors

---
+++ sda-doa/src/main/java/no/uio/ifi/localega/doa/exception/JsonSchemaValidationException.java
@@ -0,0 +1,7 @@
+package no.uio.ifi.localega.doa.exception;
+
+public class JsonSchemaValidationException extends RuntimeException {
+    public JsonSchemaValidationException(String message) {
+        super(message);
+    }
+}
