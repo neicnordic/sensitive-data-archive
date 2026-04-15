f0695535f8cd6748aca2428a1a8b1f06457f423d
Karl Grönberg <karl.gronberg@nbis.se>
2026-04-15 10:14:35 +0200

    fix(s3inbox): fix log warnings on bad requests and return generic error to user, update integration test to expect generic error, and update aws s3api cmd to use --endpoint-url arg instead of --endpoint

--- .github/integration/tests/sda/15_s3cmd-errorcodes_test.sh
+++ .github/integration/tests/sda/15_s3cmd-errorcodes_test.sh
@@ -14,8 +14,8 @@ if ! [[ "$list_all_buckets" =~ "Forbidden" ]]; then
 fi
 
 # test disallowed characters
-disallowed_characters=$(s3cmd -c s3cfg -q put s3cfg s3://test_dummy.org/fai\| 2>&1)
-if ! [[ "$disallowed_characters" =~ "filepath contains disallowed characters" ]];then
+bad_request=$(s3cmd -c s3cfg -q put s3cfg s3://test_dummy.org/fai\| 2>&1)
+if ! [[ "$bad_request" =~ "Bad Request" ]];then
     echo "test with disallowed characters failed"
     exit 1
 fi
@@ -34,13 +34,13 @@ aws configure set aws_access_key_id dummy # Value dont matter as we authenticate
 aws configure set aws_secret_access_key dummy # Value dont matter as we authenticate with the aws_session_token
 aws configure set aws_session_token "$(grep 'access_token' s3cfg | cut -d "=" -f 2 | xargs)"
 
-listResponse=$(aws s3api list-objects --endpoint http://s3inbox:8000 --bucket test_dummy.org)
+listResponse=$(aws s3api list-objects --endpoint-url http://s3inbox:8000 --bucket test_dummy.org)
 if (( $(echo "$listResponse" | grep -c "test_dummy.org") == 0 )); then
   echo "found no files for users when listing towards s3inbox with ListObjects call"
   exit 1
 fi
 
-listV2Response=$(aws s3api list-objects-v2 --endpoint http://s3inbox:8000 --bucket test_dummy.org)
+listV2Response=$(aws s3api list-objects-v2 --endpoint-url http://s3inbox:8000 --bucket test_dummy.org)
 if (( $(echo "$listV2Response" | grep -c "test_dummy.org") == 0 )); then
   echo "found no files for users when listing towards s3inbox with ListObjectsV2 call"
   exit 1
@@ -49,13 +49,13 @@ fi
 
 aws configure set aws_session_token "$(cat token)"
 
-listResponse=$(aws s3api list-objects --endpoint http://s3inbox:8000 --bucket testu_lifescience-ri.eu)
+listResponse=$(aws s3api list-objects --endpoint-url http://s3inbox:8000 --bucket testu_lifescience-ri.eu)
 if ! (( $(echo "$listResponse" | grep -c "test_dummy.org") == 0 )); then
   echo "found other users files when listing towards s3inbox with ListObjects call"
   exit 1
 fi
 
-listV2Response=$(aws s3api list-objects-v2 --endpoint http://s3inbox:8000 --bucket testu_lifescience-ri.eu)
+listV2Response=$(aws s3api list-objects-v2 --endpoint-url http://s3inbox:8000 --bucket testu_lifescience-ri.eu)
 if ! (( $(echo "$listV2Response" | grep -c "test_dummy.org") == 0 )); then
   echo "found other users files when listing towards s3inbox with ListObjectsV2 call"
   exit 1
