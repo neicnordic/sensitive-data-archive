df51674b848f9f7a99747a96de535d41a5181d89
Panos Chatzopoulos <panos.chatzopoulos@nbis.se>
2026-04-27 12:46:53 +0200

    Harden pagination and last_event trigger behavior

--- postgresql/initdb.d/02_functions.sql
+++ postgresql/initdb.d/02_functions.sql
@@ -17,18 +17,22 @@ CREATE TRIGGER files_last_modified
     EXECUTE PROCEDURE files_updated();
 
 -- Keep files.last_event in sync with file_event_log inserts
-CREATE FUNCTION update_files_last_event()
-RETURNS TRIGGER AS $update_files_last_event$
+CREATE FUNCTION sda.update_files_last_event()
+RETURNS TRIGGER
+LANGUAGE plpgsql
+SECURITY DEFINER
+SET search_path = pg_catalog, sda
+AS $update_files_last_event$
 BEGIN
     UPDATE sda.files SET last_event = NEW.event WHERE id = NEW.file_id;
     RETURN NEW;
 END;
-$update_files_last_event$ LANGUAGE plpgsql;
+$update_files_last_event$;
 
 CREATE TRIGGER file_event_log_update_last_event
     AFTER INSERT ON sda.file_event_log
     FOR EACH ROW
-    EXECUTE PROCEDURE update_files_last_event();
+    EXECUTE PROCEDURE sda.update_files_last_event();
 
 -- Function for registering files on upload
 CREATE FUNCTION sda.register_file(file_id TEXT, submission_location TEXT, submission_file_path TEXT, submission_user TEXT)
