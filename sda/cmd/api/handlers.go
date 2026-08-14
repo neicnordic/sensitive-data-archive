87f3d664c19707c5fc93ec87183421d917a32f1c
Karl Grönberg <karl.gronberg@nbis.se>
2026-08-14 14:54:50 +0200

    fix(sda): fix downloadFile & deleteFile fileId validation, fix 60_api_admin_test.sh to test invalid uuid and not found uuid

--- sda/cmd/api/handlers.go
+++ sda/cmd/api/handlers.go
@@ -573,10 +573,9 @@ func (api *API) deleteFile(w http.ResponseWriter, r *http.Request) {
 	username := r.PathValue("username")
 	fileID := r.PathValue("fileid")
 
-	span.Info("handling request to delete file", slog.String("username", username), slog.String("file_id", fileID))
-	if fileID == "" {
-		span.Error("file ID is required", nil)
-		writeJSON(w, http.StatusBadRequest, "file ID is required")
+	if _, err := uuid.Parse(fileID); err != nil {
+		span.Warn("invalid file ID")
+		writeJSON(w, http.StatusBadRequest, "invalid file id")
 
 		return
 	}
@@ -657,6 +656,13 @@ func (api *API) downloadFile(w http.ResponseWriter, r *http.Request) {
 	}
 
 	fileID := r.PathValue("fileid")
+	if _, err := uuid.Parse(fileID); err != nil {
+		span.Warn("invalid file ID")
+		writeJSON(w, http.StatusBadRequest, "invalid file id")
+
+		return
+	}
+
 	submissionUser := r.PathValue("username")
 	filePath, location, err := api.db.GetUploadedSubmissionFilePathAndLocation(ctx, submissionUser, fileID)
 	if err != nil {
