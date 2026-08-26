198ed69e827171724eec46d799fdbb1217edc805
Nanjiang Shu <nanjiang.shu@gmail.com>
2026-08-26 09:10:07 +0200

    feat(s3inbox): increase timeout to 15s - 50% of the k8s timeout
    Co-authored-by: Joakim Bygdell <joakim.bygdell@nbis.se>

--- sda/cmd/s3inbox/healthchecks.go
+++ sda/cmd/s3inbox/healthchecks.go
@@ -74,7 +74,7 @@ func (p *Proxy) CheckHealth(w http.ResponseWriter, r *http.Request) {
 // httpsGetCheck sends a request to the S3 backend and makes sure it is healthy
 func (p *Proxy) httpsGetCheck(uri string) error {
 	// Use a dedicated context timeout for readiness checks so s3inbox logs the timeout first
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
 	defer cancel()
 
 	req, err := http.NewRequestWithContext(ctx, http.MethodGet, uri, nil)
