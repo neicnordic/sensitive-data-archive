95f7ddea605242daf066f783c8834ba879d83b1e
Panos Chatzopoulos <panos.chatzopoulos@nbis.se>
2026-09-07 10:52:48 +0200

    feat(s3inbox): Rename 'deleted' to 'removed'
    Rename event 'deleted' to 'removed' and all related occurences in
    variables docs and logs

--- postgresql/migratedb.d/26_add_deleted_file_event.sql
+++ postgresql/migratedb.d/26_add_deleted_file_event.sql
@@ -5,7 +5,7 @@ DECLARE
 -- The version we know how to do migration from, at the end of a successful migration
 -- we will no longer be at this version.
   sourcever INTEGER := 25;
-  changes VARCHAR := 'Add deleted file event';
+  changes VARCHAR := 'Add removed file event';
 BEGIN
   IF (select max(version) from sda.dbschema_version) = sourcever then
     RAISE NOTICE 'Doing migration from schema version % to %', sourcever, sourcever+1;
@@ -13,7 +13,7 @@ BEGIN
     INSERT INTO sda.dbschema_version VALUES(sourcever+1, now(), changes);
 
     INSERT INTO sda.file_events(id, title, description)
-    VALUES (3, 'deleted', 'File has been deleted from the inbox storage by the user');
+    VALUES (3, 'removed', 'File has been removed from the inbox storage by the user');
 
   ELSE
     RAISE NOTICE 'Schema migration from % to % does not apply now, skipping', sourcever, sourcever+1;
