e15f1a9fcd836379ed05b629cffda881488e07b9
Panos Chatzopoulos <panos.chatzopoulos@nbis.se>
2026-09-07 10:52:48 +0200

    feat(s3inbox): Rename 'deleted' to 'removed'
    Rename event 'deleted' to 'removed' and all related occurences in
    variables docs and logs

--- postgresql/initdb.d/01_main.sql
+++ postgresql/initdb.d/01_main.sql
@@ -40,7 +40,7 @@ VALUES (0, now(), 'Created with version'),
        (23, now(), 'Expand files table with storage locations'),
        (24, now(), 'Add last_event column to files to avoid join on file_event_log'),
        (25, now(), 'Add download_path column to file_dataset to allow overriding the file submission_file_path during downloading'),
-       (26, now(), 'Add deleted file event');
+       (26, now(), 'Add removed file event');
 
 -- Datasets are used to group files, and permissions are set on the dataset
 -- level
@@ -81,7 +81,7 @@ VALUES ( 5, 'registered'  , 'Upload to the inbox has started'),
        ( 0, 'error'       , 'An Error occurred, check the error table'),
        ( 1, 'disabled'    , 'Disables the file for all actions'),
        ( 2, 'enabled'     , 'Reenables a disabled file'),
-       ( 3, 'deleted'     , 'File has been deleted from the inbox storage by the user');
+       ( 3, 'removed'     , 'File has been removed from the inbox storage by the user');
 
 -- `files` is the main table of the schema, holding the file paths, encryption
 -- header, and stable id.
